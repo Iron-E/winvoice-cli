eab9bc736a961f7098adb6216a353c25aaf42d91
Iron-E <code.iron.e@gmail.com>
2020-12-09 00:06:15 -0500

    chore(json): create templates for json structs
    These are the baseline JSON structs which will be expanded upon.

---
+++ src/json/clinvoice.rs
@@ -0,0 +1,6 @@
+use super::invoice;
+
+pub struct CLInvoice {
+	invoice: invoice::Invoice
+
+}
---
+++ src/json/mod.rs
@@ -0,0 +1,5 @@
+pub mod clinvoice;
+pub mod config;
+pub mod invoice;
+pub mod job;
+pub mod timesheet;

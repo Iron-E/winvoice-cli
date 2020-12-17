f92afa071ba9888cf9833abfea0f22773928f751
Iron-E <code.iron.e@gmail.com>
2020-12-17 02:27:39 -0500

    ref(config): create config module
    There are quite a few sections of the config file. Each should have
    their own structure.

---
+++ src/json/config/mod.rs
@@ -0,0 +1,4 @@
+pub mod clinvoice_config; // general settings
+pub mod employer_config; // employer info
+pub mod personal_config; // personal info
+pub mod time_config; // timesheet / tracking settings

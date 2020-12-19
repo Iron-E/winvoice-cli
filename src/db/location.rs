b493be7311c6c3530cd40bd4ac836b91c0fdf673
Iron-E <code.iron.e@gmail.com>
2020-12-19 03:44:28 -0500

    ref(location): prototype Display trait

--- src/db/location.rs
+++ src/db/location.rs
@@ -1,4 +1,6 @@
 use super::id::Id;
+use core::fmt::{Display, Formatter};
+use core::fmt::Result as FmtResult;
 
 /// TODO
 pub struct Location<'name>
@@ -9,4 +11,35 @@ pub struct Location<'name>
 	_outer_id: Option<Id>,
 	/// TODO
 	pub name: &'name str,
+}
+
+impl<'name> Display for Location<'name>
+{
+	fn fmt(&self, formatter: &mut Formatter<'_>) -> FmtResult
+	{
+		let output = String::from(self.name);
+
+		loop
+		{
+			// TODO
+			//
+			//	let outer = (
+			//		SELECT O
+			//		FROM Location L
+			//		JOIN Location O ON L._outer_id = O._id;
+			//	);
+			//
+			//	output::push(outer.name)
+			//
+			//	if outer._outer_id.is_none() { break; }
+
+			break;
+		}
+
+		// TODO
+		//
+		// write!(formatter, output)
+
+		todo!();
+	}
 }

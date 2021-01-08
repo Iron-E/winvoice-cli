7f4ea22a46af93556427e9c582f6e0d78acc01c1
Iron-E <code.iron.e@gmail.com>
2021-01-08 16:11:34 -0500

    fix(CrudTimesheet): remove explicit Into impl req

--- crates/adapters/clinvoice_adapter/src/data/crud_timesheet.rs
+++ crates/adapters/clinvoice_adapter/src/data/crud_timesheet.rs
@@ -1,12 +1,12 @@
 use clinvoice_data::{chrono::TimeZone, Employee, Timesheet};
 
 pub trait CrudTimesheet<'contact_info, 'email, 'objectives, 'notes, 'phone, 'work_notes, TZone> :
-	From<Timesheet<'work_notes, TZone>> + Into<Timesheet<'work_notes, TZone>> +
+	From<Timesheet<'work_notes, TZone>> +
 	Into<Employee<'contact_info, 'email, 'phone>> +
 where
 	'email : 'contact_info,
 	'phone : 'contact_info,
-	 TZone : TimeZone,
+	 TZone :  TimeZone,
 {
 
 }

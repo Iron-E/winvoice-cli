0347d7106bdbd3346e3b4271f364bca43c990a3b
Iron-E <code.iron.e@gmail.com>
2021-01-08 16:02:24 -0500

    docs(Newtype): add documentation for arms

--- crates/adapters/clinvoice_adapter/src/macros/newtype.rs
+++ crates/adapters/clinvoice_adapter/src/macros/newtype.rs
@@ -1,4 +1,26 @@
 #[macro_export]
+/// # Summary
+///
+/// `Newtype!` is a marcro which allows quick generation of wrapper types necessary to implement
+/// `Crud` traits on a CLInvoice adapter.
+///
+/// # Parameters
+///
+/// There are several arms to this macro:
+///
+/// * `Employee => $name:ident` creates new [`Employee`](clinvoice_data::Employee) wrapper.
+/// * `Invoice => $name:ident` creates new [`Invoice`](clinvoice_data::Invoice) wrapper.
+/// * `Job => $name:ident` creates new [`Job`](clinvoice_data::Job) wrapper.
+/// * `Location => $name:ident` creates new [`Job`](clinvoice_data::Job) wrapper.
+/// * `Organization => $name:ident` creates new [`Organization`](clinvoice_data::Organization) wrapper.
+/// * `Person => $name:ident` creates new [`Person`](clinvoice_data::Person) wrapper.
+/// * `Timesheet => $name:ident` creates new [`Timesheet`](clinvoice_data::Timesheet) wrapper.
+///
+/// # Examples
+///
+/// ```rust
+/// clinvoice_adapter::Newtype!(Employee => FooEmployee);
+/// ```
 macro_rules! Newtype
 {
 	(Employee => $name: ident) =>

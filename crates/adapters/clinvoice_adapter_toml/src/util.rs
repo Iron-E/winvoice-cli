cde1d2b3b72711fecd8df93417a387b9f40418cb
Iron_E <code.iron.e@gmail.com>
2021-01-24 23:46:07 -0500

    ref(util): continue work on next_id
    The function was not properly working (and still isn't). Now it is
    easier to read and returns values in a more coherent manner.

--- crates/adapters/clinvoice_adapter_toml/src/util.rs
+++ crates/adapters/clinvoice_adapter_toml/src/util.rs
@@ -39,16 +39,34 @@ pub fn create_store_dir(store_dir: &Path) -> Result<bool, io::Error>
 /// The next [`Id`] for an entity in `store_dir`.
 pub fn next_id(store_dir: &Path) -> Result<Id, Box<dyn Error>>
 {
-	let filename_conv_error = io::Error::new(
-		io::ErrorKind::InvalidInput,
-		"last file in `store_dir` has bad file name."
-	);
-
-	return Ok(match fs::read_dir(store_dir)?.last()
+	let last_dir_path = match fs::read_dir(store_dir)?.last()
 	{
-		Some(node) => node?.file_name().to_str().ok_or(filename_conv_error)?.parse::<Id>()?,
-		None => 0,
-	});
+		Some(dir_entry) => dir_entry?.path(),
+		None => return Ok(0),
+	};
+
+	println!("`last_dir_path`: {:?}", last_dir_path);
+
+	let last_dir_path_stem = match last_dir_path.file_stem()
+	{
+		Some(stem) => stem,
+		None => return Err(io::Error::new(
+			io::ErrorKind::InvalidData,
+			format!("expected `{:?}` to have a filename.", last_dir_path),
+		))?,
+	};
+
+	println!("`last_dir_id`: {:?}", last_dir_path_stem.to_str().unwrap().parse::<Id>()?);
+	println!("`last_dir_next_id`: {:?}", last_dir_path_stem.to_str().unwrap().parse::<Id>()? + 1);
+
+	return match last_dir_path_stem.to_str()
+	{
+		Some(name) => Ok(name.parse::<Id>()? + 1),
+		None => Err(io::Error::new(
+			io::ErrorKind::InvalidInput,
+			"last file in `store_dir` has bad file stem."
+		))?,
+	};
 }
 
 /// # Summary
@@ -92,4 +110,43 @@ pub fn test_temp_store(assertion: impl FnOnce(&Store<'_, '_, '_>)) -> Result<(),
 	);
 
 	return Ok(());
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::fs;
+	use std::path::PathBuf;
+
+	#[test]
+	fn test_next_id()
+	{
+		assert!(
+			super::test_temp_store(|store|
+			{
+				let test_path = PathBuf::new().join(store.path).join("test_next_id");
+
+				if test_path.is_dir()
+				{
+					assert!(fs::remove_dir_all(&test_path).is_ok());
+				}
+
+				// Create the `test_path`.
+				assert!(super::create_store_dir(&test_path).is_ok());
+
+				for i in 0..100
+				{
+					// The `next_id` matched `i`.
+					assert_eq!(super::next_id(&test_path).ok(), Some(i));
+
+					println!("next file: '{}.toml'", i);
+
+					// Creating the next file worked.
+					assert!(fs::write(&test_path.join(format!("{}.toml", i)), "").is_ok());
+
+					println!("next file: ...created.\n\n");
+				}
+			}).is_ok()
+		);
+	}
 }

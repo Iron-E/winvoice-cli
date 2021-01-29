8138fadea1e3360fbe14b8527f88da3fdfae6ad7
Iron-E <code.iron.e@gmail.com>
2021-01-28 22:13:29 -0500

    ref(clinvoice_data): allow Money to be created

--- crates/clinvoice_data/src/lib.rs
+++ crates/clinvoice_data/src/lib.rs
@@ -24,6 +24,7 @@ pub use
 };
 
 pub use chrono;
+pub use rust_decimal::Decimal;
 
 /// # Summary
 ///
--- crates/clinvoice_data/src/money.rs
+++ crates/clinvoice_data/src/money.rs
@@ -4,10 +4,39 @@ use rust_decimal::Decimal;
 #[cfg(feature="serde_support")]
 use serde::{Deserialize, Serialize};
 
+/// # Summary
+///
+/// Some `amount` of `currency`.
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature="serde_support", derive(Deserialize, Serialize))]
 pub struct Money<'currency>
 {
-	amount: Decimal,
-	currency: Cow<'currency, str>,
+	/// # Summary
+	///
+	/// The amount of `currency` that this [`Money`] represents.
+	pub amount: Decimal,
+
+	/// # Summary
+	///
+	/// The `currency` that this [`Money`] is in.
+	pub currency: Cow<'currency, str>,
+}
+
+impl<'currency> Money<'currency>
+{
+	/// # Summary
+	///
+	/// Create a new [`Money`] struct.
+	///
+	/// # Paramters
+	///
+	/// See [`Money`]'s fields.
+	///
+	/// # Returns
+	///
+	/// A new [`Money`].
+	pub fn new(amount: Decimal, currency: &'currency str) -> Self
+	{
+		return Self {amount, currency: currency.into()};
+	}
 }

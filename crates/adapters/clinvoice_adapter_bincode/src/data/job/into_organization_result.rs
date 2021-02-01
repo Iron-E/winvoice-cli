898d56077eee44fb7477ea719713465609e80fa6
Iron-E <code.iron.e@gmail.com>
2021-02-01 17:22:58 -0500

    feat(BincodeJob): impl into_organization_result

--- crates/adapters/clinvoice_adapter_bincode/src/data/job/into_organization_result.rs
+++ crates/adapters/clinvoice_adapter_bincode/src/data/job/into_organization_result.rs
@@ -1,4 +1,5 @@
-use super::BincodeJob;
+use crate::data::{BincodeJob, BincodeOrganization};
+use clinvoice_adapter::data::{Error as DataError, MatchWhen, OrganizationAdapter};
 use clinvoice_data::Organization;
 use std::error::Error;
 
@@ -6,7 +7,60 @@ impl Into<Result<Organization, Box<dyn Error>>> for BincodeJob<'_, '_, '_>
 {
 	fn into(self) -> Result<Organization, Box<dyn Error>>
 	{
-		todo!()
+		let results = BincodeOrganization::retrieve(
+			MatchWhen::EqualTo(self.job.client_id),
+			MatchWhen::Any,
+			MatchWhen::Any,
+			MatchWhen::Any,
+			self.store,
+		)?;
+
+		let bincode_organization = match results.iter().next()
+		{
+			Some(bin_org) => bin_org,
+			None => Err(DataError::DataIntegrity {id: self.job.client_id})?,
+		};
+
+		return Ok(bincode_organization.organization.clone());
 	}
 }
 
+#[cfg(test)]
+mod tests
+{
+	use super::{BincodeJob, BincodeOrganization, OrganizationAdapter};
+	use crate::util;
+	use clinvoice_adapter::data::JobAdapter;
+	use clinvoice_data::{chrono::Utc, Decimal, Id, Location, Money, Organization};
+	use std::{collections::HashSet, error::Error, time::Instant};
+
+	#[test]
+	fn test_into_organization()
+	{
+		let start = Instant::now();
+
+		util::test_temp_store(|store|
+		{
+			let dogood = BincodeOrganization::create(
+				Location {name: "Earth".into(), id: Id::new_v4(), outer_id: None},
+				"DoGood Inc",
+				HashSet::new(),
+				*store
+			).unwrap();
+
+			let test_job = BincodeJob::create(
+				dogood.organization.clone(),
+				Utc::now(),
+				Money::new(Decimal::new(200, 2), ""),
+				"Test the job creation function.",
+				*store,
+			).unwrap();
+
+			let test_org: Result<Organization, Box<dyn Error>> = test_job.into();
+
+			assert_eq!(dogood.organization, test_org.unwrap());
+
+			println!("\n>>>>> BincodeJob test_into_organization {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+		}).unwrap();
+	}
+}

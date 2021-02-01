aefcc07e19567e23849c09e3f0947126e150051f
Iron-E <code.iron.e@gmail.com>
2021-02-01 14:31:54 -0500

    feat(BincodePerson): implement Into<Person>

--- crates/adapters/clinvoice_adapter_bincode/src/data/employee/into_person_result.rs
+++ crates/adapters/clinvoice_adapter_bincode/src/data/employee/into_person_result.rs
@@ -1,4 +1,5 @@
-use super::BincodeEmployee;
+use crate::data::{BincodeEmployee, BincodePerson};
+use clinvoice_adapter::data::{Error as DataError, MatchWhen, PersonAdapter};
 use clinvoice_data::Person;
 use std::error::Error;
 
@@ -6,7 +7,64 @@ impl Into<Result<Person, Box<dyn Error>>> for BincodeEmployee<'_, '_, '_>
 {
 	fn into(self) -> Result<Person, Box<dyn Error>>
 	{
-		todo!()
+		let results = BincodePerson::retrieve(
+			MatchWhen::Any,
+			MatchWhen::EqualTo(self.employee.person_id),
+			MatchWhen::Any,
+			self.store,
+		)?;
+
+		let bincode_person = match results.iter().next()
+		{
+			Some(bin_org) => bin_org,
+			None => Err(DataError::DataIntegrity {id: self.employee.person_id})?,
+		};
+
+		return Ok(bincode_person.person.clone());
 	}
 }
 
+#[cfg(test)]
+mod tests
+{
+	use super::{BincodeEmployee, BincodePerson, PersonAdapter};
+	use crate::util;
+	use clinvoice_adapter::data::EmployeeAdapter;
+	use clinvoice_data::{Contact, Id, Organization, Person};
+	use std::{collections::HashSet, error::Error, time::Instant};
+
+	#[test]
+	fn test_into_organization()
+	{
+		let start = Instant::now();
+
+		util::test_temp_store(|store|
+		{
+			let testy = BincodePerson::create(
+				[Contact::Email("yum".into())].iter().cloned().collect(),
+				"Testy Mćtesterson".into(),
+				*store,
+			).unwrap();
+
+			let testy_employed = BincodeEmployee::create(
+				[Contact::Email("foo".into())].iter().cloned().collect(),
+				Organization
+				{
+					id: Id::new_v4(),
+					location_id: Id::new_v4(),
+					name: "DoGood Inc".into(),
+					representatives: HashSet::new(),
+				},
+				testy.person.clone(),
+				*store,
+				"CEO of Tests",
+			).unwrap();
+
+			let testy_person: Result<Person, Box<dyn Error>> = testy_employed.into();
+
+			assert_eq!(testy.person, testy_person.unwrap());
+
+			println!("\n>>>>> BincodeEmployee test_into_person {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+		}).unwrap();
+	}
+}

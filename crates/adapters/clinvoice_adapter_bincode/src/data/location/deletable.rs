04374eb00cf1ca8befcf2aefa26e2f21b5cede6e
Iron-E <code.iron.e@gmail.com>
2021-02-02 17:04:01 -0500

    feat(BincodeLocation): implement Deletable
    NOTE: tests are currently failing because `Deletable` has not been
          implemented for `BincodeOrganization`.
    
    TODO: implement `Deletable` for `BincodeOrganization`.

--- crates/adapters/clinvoice_adapter_bincode/src/data/location/deletable.rs
+++ crates/adapters/clinvoice_adapter_bincode/src/data/location/deletable.rs
@@ -1,11 +1,88 @@
 use super::BincodeLocation;
-use clinvoice_adapter::data::Deletable;
-use std::error::Error;
+use crate::data::BincodeOrganization;
+use clinvoice_adapter::data::{Deletable, LocationAdapter, MatchWhen, OrganizationAdapter};
+use std::{error::Error, fs, io::ErrorKind};
 
 impl Deletable for BincodeLocation<'_, '_, '_>
 {
 	fn delete(&self, cascade: bool) -> Result<(), Box<dyn Error>>
 	{
-		todo!()
+		if let Err(e) = fs::remove_file(self.filepath())
+		{
+			// We don't care if a file is missing; we want it deleted anyway.
+			if e.kind() != ErrorKind::NotFound
+			{
+				return Err(e.into());
+			}
+		}
+
+		if cascade
+		{
+			for result in BincodeLocation::retrieve(
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::EqualTo(Some(self.location.id)),
+				self.store,
+			)? { result.delete(true)?; }
+
+			for result in BincodeOrganization::retrieve(
+				MatchWhen::Any,
+				MatchWhen::EqualTo(self.location.id),
+				MatchWhen::Any,
+				MatchWhen::Any,
+				self.store,
+			)? { result.delete(true)?; }
+		}
+
+		return Ok(());
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::{BincodeLocation, Deletable, LocationAdapter};
+	use crate::{data::BincodeOrganization, util};
+	use clinvoice_adapter::data::OrganizationAdapter;
+	use std::{collections::HashSet, time::Instant};
+
+	#[test]
+	fn test_delete()
+	{
+		let start = Instant::now();
+
+		util::test_temp_store(|store|
+		{
+			let earth = BincodeLocation::create("Earth", *store).unwrap();
+			let usa = earth.create_inner("USA").unwrap();
+			let arizona = usa.create_inner("Arizona").unwrap();
+			let phoenix = arizona.create_inner("Phoenix").unwrap();
+
+			let dogood = BincodeOrganization::create(
+				arizona.location.clone(),
+				"DoGood Inc",
+				HashSet::new(),
+				*store
+			).unwrap();
+
+			// delete just phoenix.
+			phoenix.delete(false).unwrap();
+
+			// assert that phoenix is gone.
+			assert!(!&phoenix.filepath().is_file());
+
+			// delete earth and everything in it.
+			usa.delete(true).unwrap();
+
+			// Assert that every location inside the USA is gone
+			assert!(&earth.filepath().is_file());
+			assert!(!&usa.filepath().is_file());
+			assert!(!&arizona.filepath().is_file());
+
+			// assert that `dogood`, located in arizona, is gone.
+			assert!(!&dogood.filepath().is_file());
+
+			println!("\n>>>>> BincodeLocation test_delete {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+		}).unwrap();
 	}
 }

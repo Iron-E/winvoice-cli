235df5849d026593af9dd14d26526ed7ffae06ba
Iron-E <code.iron.e@gmail.com>
2021-02-02 17:05:05 -0500

    feat(BincodeJob): implement `Deletable`

--- crates/adapters/clinvoice_adapter_bincode/src/data/job/deletable.rs
+++ crates/adapters/clinvoice_adapter_bincode/src/data/job/deletable.rs
@@ -1,11 +1,73 @@
 use super::BincodeJob;
 use clinvoice_adapter::data::Deletable;
-use std::error::Error;
+use std::{error::Error, fs, io::ErrorKind};
 
 impl Deletable for BincodeJob<'_, '_, '_>
 {
-	fn delete(&self, cascade: bool) -> Result<(), Box<dyn Error>>
+	fn delete(&self, _cascade: bool) -> Result<(), Box<dyn Error>>
 	{
-		todo!()
+		if let Err(e) = fs::remove_file(self.filepath())
+		{
+			// We don't care if a file is missing; we want it deleted anyway.
+			if e.kind() != ErrorKind::NotFound
+			{
+				return Err(e.into());
+			}
+		}
+
+		return Ok(());
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+	use super::{BincodeJob, Deletable};
+	use crate::{data::BincodeOrganization, util};
+	use clinvoice_adapter::data::{JobAdapter, OrganizationAdapter};
+	use clinvoice_data::{chrono::Utc, Decimal, Id, Location, Money};
+	use std::{collections::HashSet, time::Instant};
+
+	#[test]
+	fn test_delete()
+	{
+		let start = Instant::now();
+
+		util::test_temp_store(|store|
+		{
+			let big_test = BincodeOrganization::create(
+				Location {id: Id::new_v4(), name: "".into(), outer_id: None},
+				"Big Old Test Corporation".into(),
+				HashSet::new(),
+				*store,
+			).unwrap();
+
+			let create_job = BincodeJob::create(
+				big_test.organization.clone(),
+				Utc::now(),
+				Money::new(Decimal::new(200, 2), ""),
+				"Test the job creation function.",
+				*store,
+			).unwrap();
+
+			let assert_job = BincodeJob::create(
+				big_test.organization.clone(),
+				Utc::now(),
+				Money::new(Decimal::new(200, 2), "USD"),
+				"Assert that this stuff works.",
+				*store,
+			).unwrap();
+
+			// Delete both jobs
+			create_job.delete(true).unwrap();
+			assert_job.delete(true).unwrap();
+
+			// Assert that all jobs are gone but the organization exists
+			assert!(!&assert_job.filepath().is_file());
+			assert!(&big_test.filepath().is_file());
+			assert!(!&create_job.filepath().is_file());
+
+			println!("\n>>>>> BincodeJob test_delete {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+		}).unwrap();
 	}
 }

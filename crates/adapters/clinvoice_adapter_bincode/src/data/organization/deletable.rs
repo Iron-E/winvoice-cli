69d0ada45e3a4cfa0f248824060bc1c4de8d25c3
Iron-E <code.iron.e@gmail.com>
2021-02-04 13:41:48 -0500

    feat(BincodeOrganization): impl `Deletable`

--- crates/adapters/clinvoice_adapter_bincode/src/data/organization/deletable.rs
+++ crates/adapters/clinvoice_adapter_bincode/src/data/organization/deletable.rs
@@ -1,14 +1,50 @@
 use
 {
 	super::BincodeOrganization,
-	clinvoice_adapter::data::Deletable,
-	std::error::Error,
+	crate::data::{BincodeEmployee, BincodeJob},
+	clinvoice_adapter::data::{Deletable, EmployeeAdapter, JobAdapter, MatchWhen},
+	std::{error::Error, fs, io::ErrorKind},
 };
 
 impl Deletable for BincodeOrganization<'_, '_, '_>
 {
 	fn delete(&self, cascade: bool) -> Result<(), Box<dyn Error>>
 	{
-		todo!()
+		if let Err(e) = fs::remove_file(self.filepath())
+		{
+			// We don't care if a file is missing; we want it deleted anyway.
+			if e.kind() != ErrorKind::NotFound
+			{
+				return Err(e.into());
+			}
+		}
+
+		if cascade
+		{
+			for result in BincodeJob::retrieve(
+				MatchWhen::EqualTo(self.organization.id),
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				self.store,
+			)? { result.delete(true)?; }
+
+			for result in BincodeEmployee::retrieve(
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::Any,
+				MatchWhen::EqualTo(self.organization.id),
+				MatchWhen::Any,
+				self.store,
+				MatchWhen::Any,
+			)? { result.delete(true)?; }
+		}
+
+		return Ok(());
 	}
 }

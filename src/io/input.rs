d78a47e39d8ed093d3263ed0aa92327290ab8731
Iron-E <code.iron.e@gmail.com>
2021-02-16 18:43:12 -0500

    fix(clinvoice io): write serialized entity to file

--- src/io/input.rs
+++ src/io/input.rs
@@ -12,7 +12,7 @@ pub fn from_editor<'de, T>(entity: T) -> Result<String, Box<dyn Error>> where
 	let temp_path_str = format!("{}.toml", temp_file_stem.path().to_string_lossy());
 	let temp_path = Path::new(&temp_path_str);
 
+	fs::write(temp_path, toml::to_string_pretty(&entity)?)?;
 	edit::edit_file(&temp_path)?;
-
 	return Ok(toml::from_slice(&fs::read(&temp_path)?)?);
 }

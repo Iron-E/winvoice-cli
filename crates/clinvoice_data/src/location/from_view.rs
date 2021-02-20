200bf72ec7de0ef4867030b3f3a9d16c6053c4ad
Iron-E <code.iron.e@gmail.com>
2021-02-20 12:25:16 -0500

    feat(Employee): impl `From<EmployeeView>`

---
+++ crates/clinvoice_data/src/location/from_view.rs
@@ -0,0 +1,22 @@
+use
+{
+	super::Location,
+	crate::views::LocationView as View,
+};
+
+impl From<View> for Location
+{
+	fn from(view: View) -> Self
+	{
+		return Self
+		{
+			id: view.id,
+			outer_id: match view.outer
+			{
+				Some(location) => Some(location.id),
+				_ => None,
+			},
+			name: view.name,
+		};
+	}
+}

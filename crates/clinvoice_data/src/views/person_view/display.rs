a7a79bf1d1c648f0b3fe0a187d61e193241434dd
Iron-E <code.iron.e@gmail.com>
2021-02-22 09:25:45 -0500

    test(PersonView): write `Display` test

--- crates/clinvoice_data/src/views/person_view/display.rs
+++ crates/clinvoice_data/src/views/person_view/display.rs
@@ -18,3 +18,78 @@ impl Display for PersonView
 	}
 }
 
+#[cfg(test)]
+mod tests
+{
+	use
+	{
+		super::PersonView,
+		crate::
+		{
+			Id,
+			views::{ContactView, LocationView}
+		},
+		std::time::Instant,
+	};
+
+	#[test]
+	fn test_display()
+	{
+		let start = Instant::now();
+
+		let earth_view = LocationView
+		{
+			name: "Earth".into(),
+			id: Id::new_v4(),
+			outer: None,
+		};
+
+		let usa_view = LocationView
+		{
+			name: "USA".into(),
+			id: Id::new_v4(),
+			outer: Some(earth_view.into()),
+		};
+
+		let arizona_view = LocationView
+		{
+			name: "Arizona".into(),
+			id: Id::new_v4(),
+			outer: Some(usa_view.into())
+		};
+
+		let phoenix_view = LocationView
+		{
+			name: "Phoenix".into(),
+			id: Id::new_v4(),
+			outer: Some(arizona_view.into()),
+		};
+
+		let street_view = LocationView
+		{
+			name: "1337 Some Street".into(),
+			id: Id::new_v4(),
+			outer: Some(phoenix_view.into()),
+		};
+
+		let contact_info = vec![
+			ContactView::Address(street_view),
+			ContactView::Email("foo@bar.io".into()),
+			ContactView::Phone("1-800-555-5555".into()),
+		];
+
+		let person_view = PersonView
+		{
+			contact_info,
+			id: Id::new_v4(),
+			name: "Someone".into(),
+		};
+
+		assert_eq!(
+			format!("{}", person_view),
+			"Contact Info:\n\t- 1337 Some Street, Phoenix, Arizona, USA, Earth\n\t- foo@bar.io\n\t- 1-800-555-5555\n\nName: Someone"
+		);
+
+		println!("\n>>>>> PersonView test_display {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+	}
+}

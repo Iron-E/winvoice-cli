236fe06112c552e51cf2afee4f64436788f4bf84
Iron-E <code.iron.e@gmail.com>
2021-02-22 09:23:12 -0500

    test(Adapters): add benchmarks

--- crates/adapters/clinvoice_adapter/src/adapters/display.rs
+++ crates/adapters/clinvoice_adapter/src/adapters/display.rs
@@ -17,11 +17,19 @@ impl Display for Adapters
 #[cfg(test)]
 mod tests
 {
-	use super::Adapters;
+	use
+	{
+		super::Adapters,
+		std::time::Instant,
+	};
 
 	#[test]
 	fn test_display()
 	{
+		let start = Instant::now();
+
 		assert_eq!(format!("{}", Adapters::Bincode), "Bincode");
+
+		println!("\n>>>>> Adapters test_display {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
 	}
 }

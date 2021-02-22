f170722f0d67ce3669502d4e64e21b790ac5681f
Iron-E <code.iron.e@gmail.com>
2021-02-22 11:31:00 -0500

    feat(OrganizationView): impl `Display`

--- crates/clinvoice_data/src/views/organization_view/display.rs
+++ crates/clinvoice_data/src/views/organization_view/display.rs
@@ -8,8 +8,49 @@ impl Display for OrganizationView
 {
 	fn fmt(&self, formatter: &mut Formatter<'_>) -> Result
 	{
-		return write!(formatter, "{:?}", self);
+		return write!(formatter, "{} @ {}", self.name, self.location);
 	}
 }
 
-// TODO
+#[cfg(test)]
+mod tests
+{
+	use
+	{
+		super::OrganizationView,
+		crate::{Id, views::LocationView},
+		std::time::Instant,
+	};
+
+	#[test]
+	fn test_display()
+	{
+		let start = Instant::now();
+
+		let organization = OrganizationView
+		{
+			id: Id::new_v4(),
+			location: LocationView
+			{
+				id: Id::new_v4(),
+				name: "Arizona".into(),
+				outer: Some(LocationView
+				{
+					id: Id::new_v4(),
+					name: "USA".into(),
+					outer: Some(LocationView
+					{
+						id: Id::new_v4(),
+						name: "Earth".into(),
+						outer: None,
+					}.into()),
+				}.into()),
+			},
+			name: "Big Old Test".into(),
+		};
+
+		assert_eq!(format!("{}", organization), "Big Old Test @ Arizona, USA, Earth");
+
+		println!("\n>>>>> OrganizationView test_display {}us <<<<<\n", Instant::now().duration_since(start).as_micros());
+	}
+}

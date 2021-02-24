92f3ba94584783499f82adb0a8d2a2ceea880751
Iron-E <code.iron.e@gmail.com>
2021-02-24 09:39:20 -0500

    feat(Adapters): impl De/Serialize

--- crates/adapters/clinvoice_adapter/src/adapters.rs
+++ crates/adapters/clinvoice_adapter/src/adapters.rs
@@ -2,10 +2,14 @@ mod display;
 
 use crate::Error;
 
+#[cfg(feature="serde_support")]
+use serde::{Deserialize, Serialize};
+
 /// # Summary
 ///
 /// Currently supported file systems / DBMS.
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[cfg_attr(feature="serde_support", derive(Deserialize, Serialize))]
 pub enum Adapters
 {
 	/// # Summary

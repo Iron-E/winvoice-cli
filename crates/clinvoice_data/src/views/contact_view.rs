0ed00e7476ae2a0d1539f96e44b5888d043cae59
Iron-E <code.iron.e@gmail.com>
2021-02-26 23:48:43 -0500

    fix(ContactView): serialize adjacently tagged enum

--- crates/clinvoice_data/src/views/contact_view.rs
+++ crates/clinvoice_data/src/views/contact_view.rs
@@ -10,6 +10,7 @@ use serde::{Deserialize, Serialize};
 /// A view of [`Location`](crate::Location).
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[cfg_attr(feature="serde_support", derive(Deserialize, Serialize))]
+#[cfg_attr(feature="serde_support", serde(content="contect", tag="type"))]
 pub enum ContactView
 {
 	/// # Summary

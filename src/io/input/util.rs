c7da6a16a7874171f4b7be7c6a6f18d96f18228d
Iron-E <code.iron.e@gmail.com>
2021-02-26 23:46:32 -0500

    fix(io input util): deserializing vec from toml

--- src/io/input/util.rs
+++ src/io/input/util.rs
@@ -11,17 +11,18 @@ use
 		Contact,
 		views::{ContactView, LocationView}
 	},
+	serde::{Deserialize, Serialize},
 };
+
+#[derive(Deserialize, Serialize)]
+struct SerdeWrapper<T> { value: T }
 
 pub fn contact_info<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -> DynamicResult<Vec<Contact>>
 	where L : LocationAdapter<'pass, 'path, 'user>
 {
-	let contact_info = super::select(
+	let mut contact_info = super::select(
 		&L::retrieve(MatchWhen::Any, MatchWhen::Any, MatchWhen::Any, store)?.into_iter().try_fold(
-			vec![
-				ContactView::Email("An email address. E.g. `foo@gmail.com`".into()),
-				ContactView::Phone("A phone number. E.g. `600-555-5555`".into()),
-			],
+			Vec::new(),
 			|mut v, l| -> DynamicResult<Vec<ContactView>>
 			{
 				let result: DynamicResult<LocationView> = l.into();
@@ -32,5 +33,8 @@ pub fn contact_info<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -
 		"Select locations to be a part of the contact info.",
 	)?;
 
-	Ok(super::edit(contact_info)?.into_iter().map(|c| c.into()).collect())
+	contact_info.push(ContactView::Email("An email address. E.g. `foo@gmail.com`".into()));
+	contact_info.push(ContactView::Phone("A phone number. E.g. `600-555-5555`".into()));
+
+	Ok(super::edit(SerdeWrapper{value: contact_info})?.value.into_iter().map(|c| c.into()).collect())
 }

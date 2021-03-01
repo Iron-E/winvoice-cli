478a144ccf6754e50035166f9ae73be34ca9c3fe
Iron-E <code.iron.e@gmail.com>
2021-03-01 17:29:56 -0500

    feat(main): implement exit_with_err

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@ use
 {
 	app::App,
 	clinvoice_config::Config,
-	std::fs,
 	structopt::StructOpt,
+	std::{error::Error, fs, process},
 };
+
+/// # Summary
+///
+/// Exit `clinvoice` with status code 1, printing some `error`.
+fn exit_with_err<E>(error: E) -> ! where E : Error
+{
+	if cfg!(debug_assertions) { panic!("{:?}", error) }
+
+	eprintln!("\nERROR: {}", error);
+	process::exit(1)
+}
 
 /// # Summary
 ///
@@ -15,12 +26,12 @@ use
 fn main()
 {
 	// Create a default user configuration if not already present.
-	Config::init().unwrap();
+	Config::init().unwrap_or_else(|e| exit_with_err(e.as_ref()));
 
 	// Get the user configuration.
-	let config_bytes = fs::read(Config::path()).unwrap();
-	let config: Config = toml::from_slice(&config_bytes).unwrap();
+	let config_bytes = fs::read(Config::path()).unwrap_or_else(|e| exit_with_err(e));
+	let config: Config = toml::from_slice(&config_bytes).unwrap_or_else(|e| exit_with_err(e));
 
 	// Run the CLInvoice application.
-	App::from_args().run(config).unwrap();
+	App::from_args().run(config).unwrap_or_else(|e| exit_with_err(e.as_ref()));
 }

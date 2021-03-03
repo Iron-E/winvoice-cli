b741252017dcb8f3ace04e4773d183e228fe2c91
Iron-E <code.iron.e@gmail.com>
2021-03-03 16:50:07 -0500

    ref(io input util): use strongly typed result

--- src/io/input/util.rs
+++ src/io/input/util.rs
@@ -1,6 +1,6 @@
 use
 {
-	super::DynamicResult,
+	crate::DynResult,
 	clinvoice_adapter::
 	{
 		data::{Error, LocationAdapter, MatchWhen},
@@ -17,8 +17,42 @@ use
 #[derive(Deserialize, Serialize)]
 struct SerdeWrapper<T> { value: T }
 
-pub fn contact_info<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -> DynamicResult<Vec<Contact>>
-	where L : LocationAdapter<'pass, 'path, 'user>
+/// # Summary
+///
+/// Retrieve all [`Location`][location]s from the specified `store` and [select](super::select)
+/// them with a `prompt`. If not [`Location`][location]s are retrieved, return an
+/// [error](Error::NoData).
+///
+/// # Errors
+///
+/// * If the [retrieval][L_retrieve] operation fails, its error is forwarded.
+/// * If no [`Location`][location]s are [retrieved][L_retrieve], an [`Error::NoData`] is returned.
+/// * If the [selection](super::select) operation fails, its error is forwarded.
+///
+/// [L_retrieve]: clinvoice_adapter::data::LocationAdapter::retrieve
+/// [location]: clinvoice_data::Location
+fn retrieve_locations_or_err<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -> DynResult<Vec<LocationView>> where
+	L : LocationAdapter<'pass, 'path, 'user>,
+{
+	let locations = L::retrieve(MatchWhen::Any, MatchWhen::Any, MatchWhen::Any, store)?;
+
+	if locations.is_empty()
+	{
+		return Err(Error::NoData {entity: stringify!(Location)}.into());
+	}
+
+	Ok(locations.into_iter().try_fold(Vec::new(),
+		|mut v, l| -> Result<Vec<LocationView>, L::Error>
+		{
+			let result: Result<LocationView, L::Error> = l.into();
+			v.push(result?);
+			Ok(v)
+		},
+	)?)
+}
+
+pub fn select_contact_info<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -> DynResult<Vec<Contact>> where
+	L : LocationAdapter<'pass, 'path, 'user>,
 {
 	let locations = super::select(
 		&retrieve_locations_or_err::<L>(store)?,
@@ -36,36 +70,12 @@ pub fn contact_info<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -
 	Ok(super::edit(SerdeWrapper {value: contact_info})?.value.into_iter().map(|c| c.into()).collect())
 }
 
-/// # Summary
-///
-/// Retrieve all [`Location`][location]s from the specified `store` and [select](super::select)
-/// them with a `prompt`. If not [`Location`][location]s are retrieved, return an
-/// [error](Error::NoData).
-///
-/// # Errors
-///
-/// * If the [retrieval][L_retrieve] operation fails, its error is forwarded.
-/// * If no [`Location`][location]s are [retrieved][L_retrieve], an [`Error::NoData`] is returned.
-/// * If the [selection](super::select) operation fails, its error is forwarded.
-///
-/// [L_retrieve]: clinvoice_adapter::data::LocationAdapter::retrieve
-/// [location]: clinvoice_data::Location
-pub fn retrieve_locations_or_err<'pass, 'path, 'user, L>(store: Store<'pass, 'path, 'user>) -> DynamicResult<Vec<LocationView>> where
+pub fn select_one_location<'pass, 'path, 'user, L, S>(prompt: S, store: Store<'pass, 'path, 'user>) -> DynResult<LocationView> where
 	L : LocationAdapter<'pass, 'path, 'user>,
+	S : Into<String>,
 {
-	let locations = L::retrieve(MatchWhen::Any, MatchWhen::Any, MatchWhen::Any, store)?;
-
-	if locations.is_empty()
-	{
-		return Err(Error::NoData {entity: stringify!(Location)}.into());
-	}
-
-	Ok(locations.into_iter().try_fold(Vec::new(),
-		|mut v, l| -> DynamicResult<Vec<LocationView>>
-		{
-			let result: DynamicResult<LocationView> = l.into();
-			v.push(result?);
-			Ok(v)
-		},
-	)?)
+	super::select_one(
+		&retrieve_locations_or_err::<L>(store)?,
+		prompt,
+	).map_err(|e| e.into())
 }

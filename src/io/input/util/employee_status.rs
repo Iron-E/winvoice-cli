04a935773b57cfbb4d944dded69f79a4aca3ca55
Iron-E <code.iron.e@gmail.com>
2021-03-10 12:02:53 -0500

    feat(io input util): impl EmployeeStatus methods

---
+++ src/io/input/util/employee_status.rs
@@ -0,0 +1,28 @@
+use
+{
+	crate::io::input,
+	clinvoice_data::EmployeeStatus,
+	std::io::Result,
+};
+
+/// # Summary
+///
+/// `prompt` the user to [select](input::select) one [`Location`][organization] from the specified `store`.
+///
+/// # Errors
+///
+/// * If [`retrieve_or_err`] fails.
+/// * If [`input::select_one`] fails.
+///
+/// [organization]: clinvoice_data::Organization
+pub fn select_one(prompt: impl Into<String>) -> Result<EmployeeStatus>
+{
+	input::select_one(
+		&[
+			EmployeeStatus::Employed,
+			EmployeeStatus::NotEmployed,
+			EmployeeStatus::Representative,
+		],
+		prompt,
+	).map_err(|e| e.into())
+}

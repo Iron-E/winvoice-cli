3cc067fb73165cc8ca2e8584b289264d3df5e0aa
Iron-E <code.iron.e@gmail.com>
2021-03-15 19:49:09 -0400

    ref(io input Error): change NotEdited Description

--- src/io/input/error.rs
+++ src/io/input/error.rs
@@ -17,7 +17,7 @@ pub enum Error
 	/// # Summary
 	///
 	/// An entity needed to be edited in order to be valid, but the user did not edit it.
-	#[snafu(display("The passed in entity was not edited by the user."))]
+	#[snafu(display("The text was not edited."))]
 	NotEdited,
 
 	#[cfg_attr(debug_assertions,      snafu(display("{:?}", err)))]

aed6a26645763395d7a4925f1b32ca7d9da8534d
Iron-E <code.iron.e@gmail.com>
2021-03-16 22:59:17 -0400

    feat(SerdeWrapper): impl `RestorableSerde`

--- src/io/input/util.rs
+++ src/io/input/util.rs
@@ -4,7 +4,20 @@ pub mod location;
 pub mod organization;
 pub mod person;
 
-use serde::{Deserialize, Serialize};
+use
+{
+	clinvoice_data::views::RestorableSerde,
+	serde::{Deserialize, Serialize},
+};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct SerdeWrapper<T> { value: T }
+
+impl<R> RestorableSerde for SerdeWrapper<R> where
+	R : RestorableSerde,
+{
+	fn restore(&mut self, original: &Self)
+	{
+		self.value.restore(&original.value);
+	}
+}

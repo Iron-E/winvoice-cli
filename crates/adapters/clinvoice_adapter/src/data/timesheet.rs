013c6dea4af1ca9bd692174c1e8cfbd1dc14dc5d
Iron-E <code.iron.e@gmail.com>
2021-04-03 14:24:33 -0400

    feat: add generic Timesheet -> Employee conversions

---
+++ crates/adapters/clinvoice_adapter/src/data/timesheet.rs
@@ -0,0 +1,29 @@
+use
+{
+	super::{EmployeeAdapter, Error, Match, query},
+	crate::Store,
+	clinvoice_data::{Employee, Timesheet},
+	std::borrow::Cow,
+};
+
+/// # Summary
+///
+/// Convert some `timesheet` into its referenced [`Employee`].
+pub fn into_employee<'store, E>(timesheet: &Timesheet, store: &'store Store)
+	-> Result<Employee, <E as EmployeeAdapter<'store>>::Error>
+where
+	E : EmployeeAdapter<'store>,
+{
+	match E::retrieve(
+		query::Employee
+		{
+			id: Match::EqualTo(Cow::Borrowed(&timesheet.employee_id)),
+			..Default::default()
+		},
+		store,
+	)?.first()
+	{
+		Some(employee) => Ok(employee.clone()),
+		_ => Err(Error::DataIntegrity {id: timesheet.employee_id}.into()),
+	}
+}

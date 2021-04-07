13b2d884c811a8ce3c9b75cae2370623703d2dca
Iron-E <code.iron.e@gmail.com>
2021-04-07 14:06:11 -0400

    fix(OuterLocation): use serde tagging
    We forgot to tag this enum, so it was giving a `ValueAfterTable` error
    on serialize.

--- crates/adapters/clinvoice_adapter/src/data/query/location/outer_location.rs
+++ crates/adapters/clinvoice_adapter/src/data/query/location/outer_location.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(feature="serde_support", derive(Deserialize, Serialize))]
+#[cfg_attr(feature="serde_support", serde(tag="type"))]
 pub enum OuterLocation<'m>
 {
 	Any,

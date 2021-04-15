28405031e3862e062e5613f22bcdaac9df9926b0
Iron-E <code.iron.e@gmail.com>
2021-04-15 18:59:38 -0400

    feat(clinvoice_export Target): add `export_job` stub

--- crates/clinvoice_export/src/target.rs
+++ crates/clinvoice_export/src/target.rs
@@ -3,6 +3,17 @@ mod from_str;
 
 pub use error::{Error, Result};
 
+use
+{
+	crate::markdown,
+
+	clinvoice_data::
+	{
+		chrono::{DateTime, Local},
+		views::JobView,
+	},
+};
+
 /// # Summary
 ///
 /// A target for exporting.
@@ -14,4 +25,37 @@ pub enum Target
 	/// The markdown target. Exports to a `.md` file.
 	#[cfg(feature="markdown")]
 	Markdown,
+}
+
+impl Target
+{
+	/// # Summary
+	///
+	/// Export some `job` to the [`Target`] specified.
+	pub fn export_job(&self, job: JobView) -> String
+	{
+		match self
+		{
+			#[cfg(feature="markdown")]
+			Self::Markdown =>
+			[
+				markdown::Element::Heading {depth: 1, text: &format!("Job #{} for {}", job.id, job.client)}.render(),
+				markdown::Element::BlockText(&format!(
+					"{} {}\n{} {}",
+					markdown::Text::Bold("Date Opened:").render(),
+					DateTime::<Local>::from(job.date_open).to_string(),
+					markdown::Text::Bold("Date Closed:").render(),
+					job.date_close.map(|date| DateTime::<Local>::from(date).to_string()).unwrap_or_else(|| "Current".into()),
+				)).render(),
+				markdown::Element::Heading {depth: 2, text: "Invoice"}.render(),
+				// TODO: fill out invoice (w/ total amount owed)
+				markdown::Element::Heading {depth: 2, text: "Objectives"}.render(),
+				// TODO: fill out objectives as a `BlockText`
+				markdown::Element::Heading {depth: 2, text: "Notes"}.render(),
+				// TODO: fill out notes as a `BlockText`
+				markdown::Element::Heading {depth: 2, text: "Timesheets"}.render(),
+				// TODO: fill out timesheets
+			].join(""),
+		}
+	}
 }

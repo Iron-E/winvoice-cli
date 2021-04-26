a5ce271362ae4e7093d55e78200778748966c72a
Iron-E <code.iron.e@gmail.com>
2021-04-26 15:32:22 -0400

    feat(MatchStr): `And`/`Or` functions
    This allows users to determine `And`/`Or` for strings.

--- crates/clinvoice_query/src/match_str.rs
+++ crates/clinvoice_query/src/match_str.rs
@@ -18,6 +18,24 @@ pub enum MatchStr<S> where S : AsRef<str>
 {
 	/// # Summary
 	///
+	/// Match if and only if all of the contained [`Match`]es also match.
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// use std::borrow::Cow::Borrowed;
+	/// use clinvoice_query::MatchStr::{And, Contains};
+	///
+	/// let and = And(vec![Contains("foo"), Contains("bar")]);
+	///
+	/// assert_eq!(and.matches("Foo"), Ok(false));
+	/// assert_eq!(and.matches("Bar"), Ok(false));
+	/// assert_eq!(and.matches("Foobar"), Ok(true));
+	/// ```
+	And(Vec<Self>),
+
+	/// # Summary
+	///
 	/// Always match.
 	Any,
 
@@ -32,7 +50,7 @@ pub enum MatchStr<S> where S : AsRef<str>
 	/// use clinvoice_query::MatchStr;
 	///
 	/// assert_eq!(MatchStr::EqualTo("Foo").matches("Foo"), Ok(true));
-	/// assert_eq!(MatchStr::EqualTo("Foo").set_matches(Iter::new(["Foo", "Bar"])), Ok(true));
+	/// assert_eq!(MatchStr::EqualTo("Foo").set_matches(&mut Iter::new(["Foo", "Bar"])), Ok(true));
 	/// ```
 	EqualTo(S),
 
@@ -51,7 +69,7 @@ pub enum MatchStr<S> where S : AsRef<str>
 	///
 	/// assert_eq!(MatchStr::Contains("Foo").matches("Foobar"), Ok(true));
 	/// assert_eq!(MatchStr::Contains("Foo").matches("barfoo"), Ok(true));
-	/// assert_eq!(MatchStr::Contains("Foo").set_matches(Iter::new(["bar", "foo"])), Ok(true));
+	/// assert_eq!(MatchStr::Contains("Foo").set_matches(&mut Iter::new(["bar", "foo"])), Ok(true));
 	/// ```
 	Contains(S),
 
@@ -62,16 +80,34 @@ pub enum MatchStr<S> where S : AsRef<str>
 	/// # Example
 	///
 	/// ```rust
-	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::MatchStr;
+	/// use std::array::IntoIter as Iter;
+	/// use clinvoice_query::MatchStr::{Contains, Not};
 	///
-	/// let not_contains = Match::Not(Match::Contains("Foo").into());
+	/// let not_contains = Not(Contains("Foo").into());
 	///
 	/// assert_eq!(not_contains.matches("Foobar"), Ok(false));
 	/// assert_eq!(not_contains.matches("barfoo"), Ok(false));
-	/// assert_eq!(not_contains.set_matches(Iter::new(["bar", "foo"])), Ok(false));
+	/// assert_eq!(not_contains.set_matches(&mut Iter::new(["bar", "foo"])), Ok(false));
 	/// ```
 	Not(Box<Self>),
+
+	/// # Summary
+	///
+	/// Match if and only if any of the contained [`Match`]es also match.
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// use std::borrow::Cow::Borrowed;
+	/// use clinvoice_query::MatchStr::{Contains, Or};
+	///
+	/// let or = Or(vec![Contains("foo"), Contains("bar")]);
+	///
+	/// assert_eq!(or.matches("Foo"), Ok(true));
+	/// assert_eq!(or.matches("Bar"), Ok(true));
+	/// assert_eq!(or.matches("Foobar"), Ok(true));
+	/// ```
+	Or(Vec<Self>),
 
 	/// # Summary
 	///
@@ -87,7 +123,7 @@ pub enum MatchStr<S> where S : AsRef<str>
 	/// use clinvoice_query::MatchStr;
 	///
 	/// assert_eq!(MatchStr::Regex("^Foo").matches("Foobar"), Ok(true));
-	/// assert_eq!(MatchStr::Regex("foo$").set_matches(Iter::new(["Bar", "foo"])), Ok(true));
+	/// assert_eq!(MatchStr::Regex("foo$").set_matches(&mut Iter::new(["Bar", "foo"])), Ok(true));
 	/// ```
 	Regex(S),
 }
@@ -110,11 +146,19 @@ impl<S> MatchStr<S> where S : AsRef<str> + Eq
 	{
 		Ok(match self
 		{
+			Self::And(matches) => matches.iter().try_fold(true, |b, m| -> Result<bool>
+			{
+				Ok(b && m.matches(value)?)
+			})?,
 			Self::Any => true,
 			Self::EqualTo(equal_value) => equal_value.as_ref() == value,
 			Self::Contains(contained_value) => value.to_lowercase().contains(&contained_value.as_ref().to_lowercase()),
 			Self::Not(m) => !m.matches(value)?,
 			Self::Regex(expression) => Regex::new(expression.as_ref())?.is_match(value),
+			Self::Or(matches) => matches.iter().try_fold(false, |b, m| -> Result<bool>
+			{
+				Ok(b || m.matches(value)?)
+			})?,
 		})
 	}
 
@@ -130,10 +174,14 @@ impl<S> MatchStr<S> where S : AsRef<str> + Eq
 	///
 	/// * `true`, if the `values` match the passed [`MatchStr`].
 	/// * `false`, if the `values` do not match.
-	pub fn set_matches<'item>(&self, mut values: impl Iterator<Item=&'item str>) -> Result<bool>
+	pub fn set_matches<'item>(&self, values: &mut impl Iterator<Item=&'item str>) -> Result<bool>
 	{
 		Ok(match self
 		{
+			Self::And(matches) => matches.iter().try_fold(true, |b, m| -> Result<bool>
+			{
+				Ok(b && m.set_matches(values.by_ref())?)
+			})?,
 			Self::Any => true,
 			Self::EqualTo(equal_value) =>
 			{
@@ -151,6 +199,10 @@ impl<S> MatchStr<S> where S : AsRef<str> + Eq
 				let regex = Regex::new(expression.as_ref())?;
 				values.any(|v| regex.is_match(v))
 			},
+			Self::Or(matches) => matches.iter().try_fold(false, |b, m| -> Result<bool>
+			{
+				Ok(b || m.set_matches(values.by_ref())?)
+			})?,
 		})
 	}
 }

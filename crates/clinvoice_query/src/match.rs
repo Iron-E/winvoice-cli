6f8a9cb08c3b85000aae769e128aed1f1aff0e18
Iron-E <code.iron.e@gmail.com>
2021-04-26 14:59:25 -0400

    feat(Match): `And`/`Or` operations

--- crates/clinvoice_query/src/match.rs
+++ crates/clinvoice_query/src/match.rs
@@ -27,6 +27,24 @@ pub enum Match<'element, T> where
 
 	/// # Summary
 	///
+	/// Match if and only if all of the contained [`Match`]es also match.
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// use std::borrow::Cow::Borrowed;
+	/// use clinvoice_query::Match::{And, EqualTo, InRange, Not};
+	///
+	/// let and = And(vec![InRange(Borrowed(&1), Borrowed(&100)), Not(EqualTo(Borrowed(&5)).into())]);
+	///
+	/// assert!(and.matches(&4));
+	/// assert!(!and.matches(&5));
+	/// assert!(and.set_matches(&([1, 2, 99].iter().collect())));
+	/// ```
+	And(Vec<Self>),
+
+	/// # Summary
+	///
 	/// Always match.
 	Any,
 
@@ -41,9 +59,9 @@ pub enum Match<'element, T> where
 	///
 	/// ```rust
 	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::Match;
+	/// use clinvoice_query::Match::EqualTo;
 	///
-	/// let equal_to = Match::EqualTo(Borrowed(&5));
+	/// let equal_to = EqualTo(Borrowed(&5));
 	///
 	/// assert!(equal_to.matches(&5));
 	/// assert!(!equal_to.matches(&4));
@@ -63,9 +81,9 @@ pub enum Match<'element, T> where
 	///
 	/// ```rust
 	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::Match;
+	/// use clinvoice_query::Match::HasAll;
 	///
-	/// let has_all = Match::HasAll(vec![Borrowed(&1), Borrowed(&5), Borrowed(&9)].into_iter().collect());
+	/// let has_all = HasAll(vec![Borrowed(&1), Borrowed(&5), Borrowed(&9)].into_iter().collect());
 	///
 	/// assert!(!has_all.matches(&1));
 	/// assert!(!has_all.matches(&3));
@@ -85,9 +103,9 @@ pub enum Match<'element, T> where
 	///
 	/// ```rust
 	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::Match;
+	/// use clinvoice_query::Match::HasAny;
 	///
-	/// let has_any = Match::HasAny(vec![Borrowed(&1), Borrowed(&5), Borrowed(&7), Borrowed(&9)].into_iter().collect());
+	/// let has_any = HasAny(vec![Borrowed(&1), Borrowed(&5), Borrowed(&7), Borrowed(&9)].into_iter().collect());
 	///
 	/// assert!(has_any.matches(&1));
 	/// assert!(!has_any.matches(&4));
@@ -106,9 +124,9 @@ pub enum Match<'element, T> where
 	///
 	/// ```rust
 	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::Match;
+	/// use clinvoice_query::Match::InRange;
 	///
-	/// let in_range = Match::InRange(Borrowed(&3), Borrowed(&5));
+	/// let in_range = InRange(Borrowed(&3), Borrowed(&5));
 	///
 	/// assert!(in_range.matches(&4));
 	/// assert!(!in_range.matches(&5));
@@ -124,9 +142,9 @@ pub enum Match<'element, T> where
 	///
 	/// ```rust
 	/// use std::borrow::Cow::Borrowed;
-	/// use clinvoice_query::Match;
+	/// use clinvoice_query::Match::{EqualTo, Not};
 	///
-	/// let not_equal_to = Match::Not(Match::EqualTo(Borrowed(&5)).into());
+	/// let not_equal_to = Not(EqualTo(Borrowed(&5)).into());
 	///
 	/// assert!(!not_equal_to.matches(&5));
 	/// assert!(not_equal_to.matches(&4));
@@ -134,6 +152,24 @@ pub enum Match<'element, T> where
 	/// assert!(not_equal_to.set_matches(&([1, 5].iter().collect())));
 	/// ```
 	Not(Box<Self>),
+
+	/// # Summary
+	///
+	/// Match if and only if all of the contained [`Match`]es also match.
+	///
+	/// # Example
+	///
+	/// ```rust
+	/// use std::borrow::Cow::Borrowed;
+	/// use clinvoice_query::Match::{EqualTo, InRange, Not, Or};
+	///
+	/// let or = Or(vec![InRange(Borrowed(&1), Borrowed(&100)), Not(EqualTo(Borrowed(&5)).into())]);
+	///
+	/// assert!(or.matches(&110));
+	/// assert!(or.matches(&5));
+	/// assert!(or.set_matches(&([1, 2, 99].iter().collect())));
+	/// ```
+	Or(Vec<Self>),
 }
 
 /// # Summary
@@ -164,12 +200,14 @@ impl<'element, T> Match<'element, T> where
 	{
 		match self
 		{
+			Self::And(matches) => matches.iter().all(|m| m.matches(value)),
 			Self::Any => true,
 			Self::EqualTo(equal_value) => equal_value.as_ref() == value,
 			Self::HasAll(required_values) => required_values.len() == 1 && required_values.contains(value),
 			Self::HasAny(accepted_values) => accepted_values.contains(value),
 			Self::InRange(min, max) => is_in_range(min.as_ref(), max.as_ref(), value),
 			Self::Not(m) => !m.matches(value),
+			Self::Or(matches) => matches.iter().any(|m| m.matches(value)),
 		}
 	}
 
@@ -189,12 +227,14 @@ impl<'element, T> Match<'element, T> where
 	{
 		match self
 		{
+			Self::And(matches) => matches.iter().all(|m| m.set_matches(values)),
 			Self::Any => true,
 			Self::EqualTo(equal_value) => values.len() == 1 && values.contains(equal_value.as_ref()),
 			Self::HasAll(required_values) => values.is_superset(&required_values.iter().map(|v| v.as_ref()).collect()),
 			Self::HasAny(accepted_values) => !values.is_disjoint(&accepted_values.iter().map(|v| v.as_ref()).collect()),
 			Self::InRange(min, max) => values.iter().any(|v| is_in_range(min.as_ref(), max.as_ref(), v)),
 			Self::Not(m) => !m.set_matches(values),
+			Self::Or(matches) => matches.iter().any(|m| m.set_matches(values)),
 		}
 	}
 }

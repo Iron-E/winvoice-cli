f5071597763914bc8f3c0007892d9de4eae0e260
Iron-E <code.iron.e@gmail.com>
2021-06-05 14:01:01 -0400

    ref(clinvoice_finance Error): add more variants
    The `Error`s from `zip` and `reqwest` have been added now, since they
    may be returned from functions in this crate.

--- crates/clinvoice_finance/src/error.rs
+++ crates/clinvoice_finance/src/error.rs
@@ -18,8 +18,14 @@ pub enum Error
 	#[error("{0}")]
 	Io(#[from] io::Error),
 
+	#[error("{0}")]
+	Reqwest(#[from] reqwest::Error),
+
 	#[error("The {0} currency is not recognized by CLInvoice. Please see https://github.com/Iron-E/clinvoice/wiki/Usage for a list of supported currencies")]
 	UnsupportedCurrency(String),
+
+	#[error("{0}")]
+	Zip(#[from] zip::result::ZipError),
 }
 
 clinvoice_error::AliasResult!();

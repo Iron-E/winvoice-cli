08b6be35f87bf7f70cbc0b46b82800705917e7b5
Iron-E <code.iron.e@gmail.com>
2021-09-20 23:21:57 -0400

    ref(app): use similar architecture to other `StructOpt`s

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,11 @@
+mod command;
 mod create;
 mod retrieve;
 mod time;
 
-use clinvoice_config::{Config, Result as ConfigResult};
+use clinvoice_config::Config;
+use command::Command;
 use create::Create;
-use dialoguer::Editor;
-use futures::future;
 use retrieve::Retrieve;
 use structopt::StructOpt;
 use time::Time;
@@ -28,71 +28,24 @@ pub struct App
 	store: String,
 
 	#[structopt(subcommand)]
-	command: AppCommand,
-}
-
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, StructOpt)]
-enum AppCommand
-{
-	#[structopt(about = "Edit the configuration file in the default editor")]
-	Config,
-
-	Create(Create),
-
-	Retrieve(Retrieve),
-
-	Time(Time),
+	command: Command,
 }
 
 impl App
 {
 	/// # Summary
 	///
-	/// Edit the user's configuration file.
-	fn edit_config(config: &Config<'_, '_>) -> ConfigResult<()>
-	{
-		let serialized = toml::to_string_pretty(config)?;
-		if let Some(edited) = Editor::new().extension(".toml").edit(&serialized)?
-		{
-			let deserialized: Config = toml::from_str(&edited)?;
-			deserialized.update()?;
-		}
-
-		Ok(())
-	}
-
-	/// # Summary
-	///
 	/// Run the application and parse its provided arguments / flags.
 	pub async fn run<'err>(self, config: Config<'_, '_>) -> DynResult<'err, ()>
 	{
-		let store = config
-			.get_store(&self.store)
-			.expect("Storage name not known");
-
-		match self.command
-		{
-			AppCommand::Config =>
-			{
-				match Self::edit_config(&config)
-				{
-					Ok(_) => future::ok(()),
-					Err(e) => future::err(e.into()),
-				}
-				.await
-			},
-			AppCommand::Create(cmd) => cmd.run(config.invoices.default_currency, store).await,
-			AppCommand::Retrieve(cmd) => cmd.run(&config, store).await,
-			AppCommand::Time(cmd) =>
-			{
-				cmd.run(
-					config.invoices.default_currency,
-					config.employees.default_id,
-					config.timesheets.interval,
-					store,
-				)
-				.await
-			},
-		}
+		self
+			.command
+			.run(
+				&config,
+				config
+					.get_store(&self.store)
+					.expect("Storage name not known"),
+			)
+			.await
 	}
 }
---
+++ src/app/command.rs
@@ -0,0 +1,70 @@
+use clinvoice_adapter::Store;
+use clinvoice_config::{Config, Result as ConfigResult};
+use dialoguer::Editor;
+use futures::future;
+use structopt::StructOpt;
+
+use super::{Create, Retrieve, Time};
+use crate::DynResult;
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, StructOpt)]
+pub enum Command
+{
+	#[structopt(about = "Edit the configuration file in the default editor")]
+	Config,
+
+	Create(Create),
+
+	Retrieve(Retrieve),
+
+	Time(Time),
+}
+
+impl Command
+{
+	/// # Summary
+	///
+	/// Edit the user's configuration file.
+	fn edit_config(config: &Config<'_, '_>) -> ConfigResult<()>
+	{
+		let serialized = toml::to_string_pretty(config)?;
+		if let Some(edited) = Editor::new().extension(".toml").edit(&serialized)?
+		{
+			let deserialized: Config = toml::from_str(&edited)?;
+			deserialized.update()?;
+		}
+
+		Ok(())
+	}
+
+	/// # Summary
+	///
+	/// Run the application and parse its provided arguments / flags.
+	pub async fn run<'err>(self, config: &Config<'_, '_>, store: &Store) -> DynResult<'err, ()>
+	{
+		match self
+		{
+			Self::Config =>
+			{
+				match Self::edit_config(&config)
+				{
+					Ok(_) => future::ok(()),
+					Err(e) => future::err(e.into()),
+				}
+				.await
+			},
+			Self::Create(cmd) => cmd.run(config.invoices.default_currency, store).await,
+			Self::Retrieve(cmd) => cmd.run(&config, store).await,
+			Self::Time(cmd) =>
+			{
+				cmd.run(
+					config.invoices.default_currency,
+					config.employees.default_id,
+					config.timesheets.interval,
+					store,
+				)
+				.await
+			},
+		}
+	}
+}

3436b0550c39c30fca78b6391ae559040a083350
Iron-E <code.iron.e@gmail.com>
2021-10-14 13:06:32 -0400

    test(clinvoice_config): fix reference to renamed field

--- crates/clinvoice_config/src/timesheets.rs
+++ crates/clinvoice_config/src/timesheets.rs
@@ -23,7 +23,7 @@ pub struct Timesheets
 	///
 	/// // 5 minute increment
 	/// Timesheets {
-	/// 	increment: Duration::new(300, 0),
+	/// 	default_increment: Duration::new(300, 0),
 	/// };
 	/// ```
 	#[serde(with = "humantime_serde")]

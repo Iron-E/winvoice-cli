fe88d00a271214eb8220e0b24cb22218ffe27d07
Iron-E <code.iron.e@gmail.com>
2021-11-05 20:13:51 -0400

    ref(PostgresPerson): use `Schema::write_query`

--- crates/adapters/clinvoice_adapter_postgres/src/schema/person/person_adapter.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/person/person_adapter.rs
@@ -1,12 +1,13 @@
-use std::{fmt::Write, ops::Deref};
+use std::fmt::Write;
 
-use clinvoice_adapter::schema::PersonAdapter;
-use clinvoice_query::{Person as QueryPerson, Match, MatchStr};
+use clinvoice_adapter::{schema::PersonAdapter, WriteSql};
+use clinvoice_query as query;
 use clinvoice_schema::{views::PersonView, Person};
 use futures::stream::TryStreamExt;
 use sqlx::{postgres::Postgres, Executor, Result, Row};
 
 use super::PostgresPerson;
+use crate::PostgresSchema;
 
 #[async_trait::async_trait]
 impl PersonAdapter for PostgresPerson
@@ -25,93 +26,27 @@ impl PersonAdapter for PostgresPerson
 
 	async fn retrieve_view(
 		connection: impl 'async_trait + Executor<'_, Database = Postgres>,
-		query: &QueryPerson,
+		query: &query::Person,
 	) -> Result<Vec<PersonView>>
 	{
 		let mut sql = String::with_capacity(21);
 		write!(sql, "SELECT * FROM people").unwrap();
 
-		if query != &Default::default()
-		{
-			write!(sql, " WHERE").unwrap();
-		}
+		const PREFIX_WHERE_CLAUSE: Option<&str> = Some("WHERE");
 
-		if query.id != Match::Any
-		{
-			fn write_query(prefix: Option<&str>, query: &Match<'_, i64>, sql: &mut String)
+		PostgresSchema::write_where(
+			"name",
+			if PostgresSchema::write_where("id", PREFIX_WHERE_CLAUSE, &query.id, &mut sql)
 			{
-				// TODO: make `ToSql` trait in `clinvoice_adapter` with `impl` on `Schema`
-				match query
-				{
-					Match::AllGreaterThan(id) | Match::GreaterThan(id) =>
-					{
-						write!(sql, " {} id > {}", prefix.unwrap_or_default(), id).unwrap()
-					},
-					Match::AllLessThan(id) | Match::LessThan(id) =>
-					{
-						write!(sql, " {} id < {}", prefix.unwrap_or_default(), id).unwrap()
-					},
-					Match::AllInRange(low, high) | Match::InRange(low, high) => write!(
-						sql,
-						" {} {} <= id AND id < {}",
-						prefix.unwrap_or_default(),
-						low,
-						high
-					)
-					.unwrap(),
-					Match::And(queries) =>
-					{
-						prefix.map(|p| write!(sql, " {}", p).unwrap());
-						queries.first().map(|q| write_query(Some("("), q, sql));
-						queries
-							.iter()
-							.skip(1)
-							.for_each(|q| write_query(Some("AND"), q, sql));
-						write!(sql, ")").unwrap();
-					},
-					Match::EqualTo(id) =>
-					{
-						write!(sql, " {} id = {}", prefix.unwrap_or_default(), id).unwrap()
-					},
-					Match::HasAll(ids) =>
-					{
-						let mut iter = ids.iter();
-						iter.next().map(|id| {
-							write!(sql, " {} id = ALL(ARRAY[{}", prefix.unwrap_or_default(), id).unwrap()
-						});
-						iter.for_each(|id| write!(sql, ", {}", id).unwrap());
-						write!(sql, "])").unwrap();
-					},
-					Match::HasAny(ids) =>
-					{
-						let mut iter = ids.iter();
-						iter.next().map(|id| {
-							write!(sql, " {} id IN ({}", prefix.unwrap_or_default(), id).unwrap()
-						});
-						iter.for_each(|id| write!(sql, ", {}", id).unwrap());
-						write!(sql, ")").unwrap();
-					},
-					Match::Not(positive_query) =>
-					{
-						prefix.map(|p| write!(sql, " {}", p).unwrap());
-						write_query(Some("NOT ("), positive_query.deref(), sql);
-						write!(sql, ") ").unwrap();
-					},
-					Match::Or(queries) =>
-					{
-						prefix.map(|p| write!(sql, " {}", p).unwrap());
-						queries.first().map(|q| write_query(Some("("), q, sql));
-						queries
-							.iter()
-							.skip(1)
-							.for_each(|q| write_query(Some("OR"), q, sql));
-						write!(sql, ")").unwrap();
-					},
-					_ => unreachable!("`query.id` is not supposed to be `Any` by this point"),
-				}
+				None
 			}
-			write_query(None, &query.id, &mut sql);
-		};
+			else
+			{
+				PREFIX_WHERE_CLAUSE
+			},
+			&query.name,
+			&mut sql,
+		);
 
 		sql.push(';');
 		sqlx::query(&sql)

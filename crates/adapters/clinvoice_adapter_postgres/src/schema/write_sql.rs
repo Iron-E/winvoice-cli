14e86a9bbe878b928b1006926e9564a0bcbd7715
Iron-E <code.iron.e@gmail.com>
2021-11-09 10:06:46 -0500

    ref(Schema): impl `WriteSql` in terms of methods

--- crates/adapters/clinvoice_adapter_postgres/src/schema/write_sql.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/write_sql.rs
@@ -1,12 +1,10 @@
 use std::{
-	fmt::{Debug, Write},
-	hash::Hash,
+	fmt::{Display, Write},
 	ops::Deref,
 };
 
 use clinvoice_adapter::WriteSql;
 use clinvoice_query::{Match, MatchStr};
-use clinvoice_schema::EmployeeStatus;
 
 use super::PostgresSchema;
 
@@ -27,8 +25,8 @@ fn write_boolean_group<Q>(
 	prefix: Option<&'static str>,
 	column: &'static str,
 	queries: &[Q],
-	sql: &mut String,
 	union: bool,
+	sql: &mut String,
 ) where
 	PostgresSchema: WriteSql<Q>,
 {
@@ -45,17 +43,77 @@ fn write_boolean_group<Q>(
 
 /// # Summary
 ///
-/// Write multiple `AND`/`OR` `queries`.
+/// Write a comparison of `column` and `comparand` using `comparator`.
 ///
-/// * If `union` is `true`, the `queries` are separated by `AND`:
-///   `[Match::EqualTo(3), Match::LessThan(4)]` is interpreted as `(foo = 3 AND foo < 4)`.
-/// * If `union` is `false`, the `queries` are separated by `OR`:
-///   `[Match::EqualTo(3), Match::LessThan(4)]` is interpreted as `(foo = 3 OR foo < 4)`.
+/// The rest of the args are the same as [`WriteSql::write_where`].
+fn write_comparison(
+	prefix: Option<&'static str>,
+	column: &'static str,
+	comperator: &'static str,
+	comparand: impl Display,
+	sql: &mut String,
+)
+{
+	write!(
+		sql,
+		" {} {} {} {}",
+		prefix.unwrap_or_default(),
+		column,
+		comperator,
+		comparand
+	)
+	.unwrap()
+}
+
+/// # Summary
+///
+/// Check if some `column` has `ANY` or `ALL` of the `values` provided.
+///
+/// * If `union` is `true`, a check is done to see if `ALL` of the `values` are `column`.
+/// * If `union` is `false`, a check is done to see if `ANY` of the `values` are `column`.
 ///
 /// NOTE: the above is not proper [`Match`] syntax, since they need to wrap their inner value in a
 ///       [`std::borrow::Cow`]. View the linked documentation for proper examples.
 ///
 /// The rest of the args are the same as [`WriteSql::write_where`].
+fn write_has<'t, T>(
+	prefix: Option<&'static str>,
+	column: &'static str,
+	values: impl IntoIterator<Item = &'t T>,
+	union: bool,
+	sql: &mut String,
+) where
+	T: 't + Display,
+{
+	let mut iter = values.into_iter();
+	iter.next().map(|id| {
+		write!(
+			sql,
+			" {} {} = {}{}",
+			if union { "ALL(ARRAY[" } else { "IN (" },
+			prefix.unwrap_or_default(),
+			column,
+			id
+		)
+		.unwrap()
+	});
+	iter.for_each(|id| write!(sql, ", {}", id).unwrap());
+	if union
+	{
+		write!(sql, "])")
+	}
+	else
+	{
+		write!(sql, ")")
+	}
+	.unwrap()
+}
+
+/// # Summary
+///
+/// Wrap some `query` in `NOT (…)`.
+///
+/// The args are the same as [`WriteSql::write_where`].
 fn write_negated<Q>(
 	prefix: Option<&'static str>,
 	column: &'static str,
@@ -82,100 +140,24 @@ impl WriteSql<Match<'_, i64>> for PostgresSchema
 		{
 			Match::AllGreaterThan(id) | Match::GreaterThan(id) =>
 			{
-				write!(sql, " {} {} > {}", prefix.unwrap_or_default(), column, id).unwrap()
+				write_comparison(prefix, column, ">", id, sql)
 			},
 			Match::AllLessThan(id) | Match::LessThan(id) =>
 			{
-				write!(sql, " {} {} < {}", prefix.unwrap_or_default(), column, id).unwrap()
+				write_comparison(prefix, column, "<", id, sql)
 			},
-			Match::AllInRange(low, high) | Match::InRange(low, high) => write!(
-				sql,
-				" {} {} <= {column} AND {column} < {}",
-				prefix.unwrap_or_default(),
-				low,
-				high,
-				column = column,
-			)
-			.unwrap(),
-			Match::And(queries) => write_boolean_group(prefix, column, queries, sql, false),
+			Match::AllInRange(low, high) | Match::InRange(low, high) =>
+			{
+				write_comparison(prefix, column, ">=", low, sql);
+				write_comparison(Some("AND"), column, "<", high, sql);
+			},
+			Match::And(queries) => write_boolean_group(prefix, column, queries, true, sql),
 			Match::Any => return false,
-			Match::EqualTo(id) => write!(sql, " {} {} = {}", prefix.unwrap_or_default(), column, id).unwrap(),
-			Match::HasAll(ids) =>
-			{
-				let mut iter = ids.iter();
-				iter.next().map(|id| {
-					write!(sql, " {} {} = ALL(ARRAY[{}", prefix.unwrap_or_default(), column, id).unwrap()
-				});
-				iter.for_each(|id| write!(sql, ", {}", id).unwrap());
-				write!(sql, "])").unwrap();
-			},
-			Match::HasAny(ids) =>
-			{
-				let mut iter = ids.iter();
-				iter
-					.next()
-					.map(|id| write!(sql, " {} {} IN ({}", prefix.unwrap_or_default(), column, id).unwrap());
-				iter.for_each(|id| write!(sql, ", {}", id).unwrap());
-				write!(sql, ")").unwrap();
-			},
+			Match::EqualTo(id) => write_comparison(prefix, column, "=", id, sql),
+			Match::HasAll(ids) => write_has(prefix, column, ids, true, sql),
+			Match::HasAny(ids) => write_has(prefix, column, ids, false, sql),
 			Match::Not(query) => write_negated(prefix, column, query, sql),
-			Match::Or(queries) => write_boolean_group(prefix, column, queries, sql, false),
-		};
-		true
-	}
-}
-
-impl WriteSql<Match<'_, EmployeeStatus>> for PostgresSchema
-{
-	fn write_where(
-		prefix: Option<&'static str>,
-		column: &'static str,
-		query: &Match<'_, EmployeeStatus>,
-		sql: &mut String,
-	) -> bool
-	{
-		match query
-		{
-			Match::AllGreaterThan(status) | Match::GreaterThan(status) =>
-			{
-				write!(sql, " {} {} > '{}'", prefix.unwrap_or_default(), column, status).unwrap()
-			},
-			Match::AllLessThan(status) | Match::LessThan(status) =>
-			{
-				write!(sql, " {} {} < '{}'", prefix.unwrap_or_default(), column, status).unwrap()
-			},
-			Match::AllInRange(low, high) | Match::InRange(low, high) => write!(
-				sql,
-				" {} {} <= '{column}' AND '{column}' < {}",
-				prefix.unwrap_or_default(),
-				low,
-				high,
-				column = column,
-			)
-			.unwrap(),
-			Match::And(queries) => write_boolean_group(prefix, column, queries, sql, false),
-			Match::Any => return false,
-			Match::EqualTo(status) => write!(sql, " {} {} = '{}'", prefix.unwrap_or_default(), column, status).unwrap(),
-			Match::HasAll(ids) =>
-			{
-				let mut iter = ids.iter();
-				iter.next().map(|status| {
-					write!(sql, " {} {} = ALL(ARRAY['{}'", prefix.unwrap_or_default(), column, status).unwrap()
-				});
-				iter.for_each(|status| write!(sql, ", '{}'", status).unwrap());
-				write!(sql, "])").unwrap();
-			},
-			Match::HasAny(ids) =>
-			{
-				let mut iter = ids.iter();
-				iter
-					.next()
-					.map(|status| write!(sql, " {} {} IN ('{}'", prefix.unwrap_or_default(), column, status).unwrap());
-				iter.for_each(|status| write!(sql, ", '{}'", status).unwrap());
-				write!(sql, ")").unwrap();
-			},
-			Match::Not(query) => write_negated(prefix, column, query, sql),
-			Match::Or(queries) => write_boolean_group(prefix, column, queries, sql, false),
+			Match::Or(queries) => write_boolean_group(prefix, column, queries, false, sql),
 		};
 		true
 	}
@@ -192,7 +174,7 @@ impl WriteSql<MatchStr<String>> for PostgresSchema
 	{
 		match query
 		{
-			MatchStr::And(queries) => write_boolean_group(prefix, column, queries, sql, false),
+			MatchStr::And(queries) => write_boolean_group(prefix, column, queries, true, sql),
 			MatchStr::Any => return false,
 			MatchStr::Contains(string) => write!(
 				sql,
@@ -200,23 +182,12 @@ impl WriteSql<MatchStr<String>> for PostgresSchema
 				prefix.unwrap_or_default(),
 				column,
 				string,
-			).unwrap(),
-			MatchStr::EqualTo(string) => write!(
-				sql,
-				" {} {} = {}",
-				prefix.unwrap_or_default(),
-				column,
-				string,
-			).unwrap(),
+			)
+			.unwrap(),
+			MatchStr::EqualTo(string) => write_comparison(prefix, column, "=", string, sql),
 			MatchStr::Not(query) => write_negated(prefix, column, query, sql),
-			MatchStr::Or(queries) => write_boolean_group(prefix, column, queries, sql, false),
-			MatchStr::Regex(regex) => write!(
-				sql,
-				" {} {} ~ {}",
-				prefix.unwrap_or_default(),
-				column,
-				regex,
-			).unwrap(),
+			MatchStr::Or(queries) => write_boolean_group(prefix, column, queries, false, sql),
+			MatchStr::Regex(regex) => write_comparison(prefix, column, "~", regex, sql),
 		};
 		true
 	}

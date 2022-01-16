1eb8d00e9e1108cc6a09d8aac1b5b8e8d6111237
Iron-E <code.iron.e@gmail.com>
2022-01-16 12:46:36 -0500

    style: manually indent instead of `\` linebreak

--- crates/adapters/clinvoice_adapter_postgres/src/schema/job/job_adapter.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/job/job_adapter.rs
@@ -78,8 +78,8 @@ impl JobAdapter for PgJob
 
 		let mut query = String::from(
 			"SELECT
-				J.id, J.client_id, J.date_close, J.date_open, J.increment, J.invoice_date_issued, \
-			 J.invoice_date_paid, J.invoice_hourly_rate, J.notes, J.objectives,
+				J.id, J.client_id, J.date_close, J.date_open, J.increment, J.invoice_date_issued,
+					J.invoice_date_paid, J.invoice_hourly_rate, J.notes, J.objectives,
 				O.name, O.location_id
 			FROM jobs J
 			JOIN organizations O ON (O.id = J.client_id)",

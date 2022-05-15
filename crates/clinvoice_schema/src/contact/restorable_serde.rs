3fad409aece04fe75989f969334df0944b6a5a58
Iron-E <code.iron.e@gmail.com>
2022-05-15 18:18:01 -0400

    fix(Contact): restore `employee_id`

--- crates/clinvoice_schema/src/contact/restorable_serde.rs
+++ crates/clinvoice_schema/src/contact/restorable_serde.rs
@@ -5,13 +5,14 @@ impl RestorableSerde for Contact
 {
 	fn try_restore(&mut self, original: &Self) -> RestoreResult<()>
 	{
+		self.employee_id = original.employee_id;
 		if let ContactKind::Address(ref mut location) = self.kind
 		{
 			match original.kind
 			{
 				ContactKind::Address(ref original_location) =>
 				{
-					location.try_restore(&original_location)?
+					location.try_restore(original_location)?
 				},
 				_ => return Err(RestoreError),
 			}

b350d352e91896f30180462f9e63a768ab1e8906
Iron-E <code.iron.e@gmail.com>
2022-05-25 15:11:49 -0400

    chore(PgTimesheetColumns): remove unused columns

--- crates/adapters/clinvoice_adapter_postgres/src/schema/timesheet/columns.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/timesheet/columns.rs
@@ -1,13 +1,9 @@
 use clinvoice_schema::{Expense, Timesheet, Employee, Job};
 use sqlx::{postgres::PgRow, Result, Row};
 
-use crate::schema::{employee::columns::PgEmployeeColumns, job::columns::PgJobColumns};
-
 pub(in crate::schema) struct PgTimesheetColumns<'col>
 {
 	pub id: &'col str,
-	pub employee: PgEmployeeColumns<'col>,
-	pub job: PgJobColumns<'col>,
 	pub time_begin: &'col str,
 	pub time_end: &'col str,
 	pub work_notes: &'col str,

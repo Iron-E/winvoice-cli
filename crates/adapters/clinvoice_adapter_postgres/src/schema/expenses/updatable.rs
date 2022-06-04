56ef255a75a7fe75446ed069aff23bdfe387c440
Iron-E <code.iron.e@gmail.com>
2022-06-04 16:03:56 -0400

    ref(clinvoice_adapter_postgres): impl more `Updatable`s

--- crates/adapters/clinvoice_adapter_postgres/src/schema/expenses/updatable.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/expenses/updatable.rs
@@ -1,6 +1,6 @@
-use clinvoice_adapter::Updatable;
+use clinvoice_adapter::{Updatable, schema::columns::ExpenseColumns};
 use clinvoice_schema::Expense;
-use sqlx::{Postgres, Result, Transaction};
+use sqlx::{Postgres, Result, Transaction, QueryBuilder};
 
 use super::PgExpenses;
 
@@ -18,7 +18,74 @@ impl Updatable for PgExpenses
 		'e: 'i,
 		Self::Entity: 'e,
 	{
-		todo!()
+		const COLUMNS: ExpenseColumns<&'static str> = ExpenseColumns::default();
+		const TABLE_IDENT: &'static str = "X";
+		const VALUES_IDENT: &'static str = "V";
+
+		let mut peekable_entities = entities.clone().peekable();
+
+		// There is nothing to do.
+		if peekable_entities.peek().is_none()
+		{
+			return Ok(());
+		}
+
+		let values_columns = COLUMNS.scoped(VALUES_IDENT);
+
+		let mut query = QueryBuilder::new("UPDATE expenses AS ");
+
+		query
+			.separated(' ')
+			.push(TABLE_IDENT)
+			.push("SET")
+			.push(COLUMNS.category)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.category)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.cost)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.cost)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.description)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.description)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.timesheet_id)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.timesheet_id)
+			.push("FROM (");
+
+		query.push_values(peekable_entities, |mut q, e| {
+			q.push_bind(&e.category)
+				.push_bind(e.cost.amount.to_string())
+				.push_bind(&e.description)
+				.push_bind(e.id)
+				.push_bind(e.timesheet_id);
+		});
+
+		query
+			.separated(' ')
+			.push(") AS")
+			.push(VALUES_IDENT)
+			.push('(')
+			.push_unseparated(COLUMNS.category)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.cost)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.description)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.timesheet_id)
+			.push_unseparated(')')
+			.push("WHERE")
+			.push(COLUMNS.scoped(TABLE_IDENT).id)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.id);
+
+		query.push(';').build().execute(connection).await?;
+
+		Ok(())
 	}
 }
 
--- crates/adapters/clinvoice_adapter_postgres/src/schema/timesheet/updatable.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/timesheet/updatable.rs
@@ -1,6 +1,8 @@
-use clinvoice_adapter::Updatable;
+use clinvoice_adapter::{Updatable, schema::columns::TimesheetColumns};
 use clinvoice_schema::Timesheet;
-use sqlx::{Postgres, Result, Transaction};
+use sqlx::{Postgres, Result, Transaction, QueryBuilder};
+
+use crate::schema::{PgEmployee, PgJob};
 
 use super::PgTimesheet;
 
@@ -18,7 +20,85 @@ impl Updatable for PgTimesheet
 		'e: 'i,
 		Self::Entity: 'e,
 	{
-		todo!()
+		const COLUMNS: TimesheetColumns<&'static str> = TimesheetColumns::default();
+		const TABLE_IDENT: &'static str = "O";
+		const VALUES_IDENT: &'static str = "V";
+
+		let mut peekable_entities = entities.clone().peekable();
+
+		// There is nothing to do.
+		if peekable_entities.peek().is_none()
+		{
+			return Ok(());
+		}
+
+		let values_columns = COLUMNS.scoped(VALUES_IDENT);
+
+		let mut query = QueryBuilder::new("UPDATE timesheets AS ");
+
+		query
+			.separated(' ')
+			.push(TABLE_IDENT)
+			.push("SET")
+			.push(COLUMNS.employee_id)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.employee_id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.job_id)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.job_id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.time_begin)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.time_begin)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.time_end)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.time_end)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.work_notes)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.work_notes)
+			.push("FROM (");
+
+		query.push_values(peekable_entities, |mut q, e| {
+			q.push_bind(e.employee.id)
+				.push_bind(e.id)
+				.push_bind(e.job.id)
+				.push_bind(e.time_begin)
+				.push_bind(e.time_end)
+				.push_bind(&e.work_notes);
+		});
+
+		query
+			.separated(' ')
+			.push(") AS")
+			.push(VALUES_IDENT)
+			.push('(')
+			.push_unseparated(COLUMNS.employee_id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.job_id)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.time_begin)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.time_end)
+			.push_unseparated(',')
+			.push_unseparated(COLUMNS.work_notes)
+			.push_unseparated(')')
+			.push("WHERE")
+			.push(COLUMNS.scoped(TABLE_IDENT).id)
+			.push_unseparated('=')
+			.push_unseparated(values_columns.id);
+
+		query.push(';').build().execute(&mut *connection).await?;
+
+		let employees = entities.clone().map(|e| &e.employee);
+		PgEmployee::update(connection, employees).await?;
+		PgJob::update(connection, entities.map(|e| &e.job)).await?;
+
+		Ok(())
 	}
 }
 

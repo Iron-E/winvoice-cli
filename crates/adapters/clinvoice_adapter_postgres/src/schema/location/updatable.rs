9b4c13cd7cf77e4e81327daf4c2cc2ffdcf952a9
Iron-E <code.iron.e@gmail.com>
2022-06-08 18:14:33 -0400

    perf(PgLocation): inscrease performance of `PgLocation::update`
    There are several techniques used to do this. Firstly, we use a
    `LinkedList` to store the `Vec`s of `Location`s since we don't know how
    many `Vec`s there will be. Second, we don't collect the `outer`s into a
    `Vec` in the first place until we know that therer are any.

--- crates/adapters/clinvoice_adapter_postgres/src/schema/location/updatable.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/location/updatable.rs
@@ -1,3 +1,5 @@
+use std::collections::LinkedList;
+
 use clinvoice_adapter::{schema::columns::LocationColumns, Updatable};
 use clinvoice_schema::Location;
 use sqlx::{Postgres, Result, Transaction};
@@ -19,17 +21,55 @@ impl Updatable for PgLocation
 		'e: 'i,
 		Self::Entity: 'e,
 	{
-		let mut peekable_entities = entities.clone().peekable();
+		let mut entities_peekable = entities.peekable();
 
 		// There is nothing to do.
-		if peekable_entities.peek().is_none()
+		if entities_peekable.peek().is_none()
 		{
 			return Ok(());
 		}
 
+		let mut entities_by_outer: LinkedList<Vec<_>> = Default::default();
+		entities_by_outer.push_back(entities_peekable.collect());
+
+		loop
+		{
+			let mut outers = entities_by_outer
+				.back()
+				.unwrap()
+				.iter()
+				.filter_map(|e| e.outer.as_deref())
+				.peekable();
+
+			// There are no more outer locations, so we can stop looking for them in this loop.
+			if outers.peek().is_none()
+			{
+				break;
+			}
+
+			let outers_collected = outers.collect();
+			entities_by_outer.push_back(outers_collected);
+		}
+
+		let mut entities_collected: Vec<_> = entities_by_outer.into_iter().flatten().collect();
+
+		// NOTE: we don't want to update a given row in the DB more than once.
+		// PERF: we can only get duplicates if there is more than one entitiy to update.
+		if entities_collected.len() > 1
+		{
+			// PERF: `dedup` needs a list to be sorted. there's no way for two duplicates to get
+			//       unsorted unless there are at least three elements.
+			if entities_collected.len() > 2
+			{
+				entities_collected.sort();
+			}
+
+			entities_collected.dedup();
+		}
+
 		const COLUMNS: LocationColumns<&'static str> = LocationColumns::default();
 		PgSchema::update(&mut *connection, COLUMNS, "locations", "L", "V", |query| {
-			query.push_values(peekable_entities, |mut q, e| {
+			query.push_values(entities_collected.iter(), |mut q, e| {
 				q.push_bind(e.id)
 					.push_bind(&e.name)
 					.push_bind(e.outer.as_ref().map(|o| o.id));
@@ -37,7 +77,10 @@ impl Updatable for PgLocation
 		})
 		.await?;
 
-		Self::update(connection, entities.filter_map(|e| e.outer.as_deref())).await?;
+		// TODO: make this function recursive once `async` traits are stable.
+		//       at that point, it will be possible to remove `entities_collected` and instead use
+		//       `entities.clone().peekable()` in its place.
+		// Self::update(connection, entities.filter_map(|e| e.outer.as_deref())).await?;
 
 		Ok(())
 	}
@@ -61,7 +104,7 @@ mod tests
 			.await
 			.unwrap();
 
-		let usa = PgLocation::create(&connection, "USA".into(), Some(earth.clone()))
+		let mut usa = PgLocation::create(&connection, "USA".into(), Some(earth.clone()))
 			.await
 			.unwrap();
 
@@ -71,11 +114,11 @@ mod tests
 		)
 		.unwrap();
 
+		usa.name = "Not USA".into();
 		arizona.name = "Not Arizona".into();
-		utah.outer = utah.outer.map(|mut o| {
-			o.name = "Not USA".into();
-			o
-		});
+
+		arizona.outer = Some(usa.clone().into());
+		utah.outer = Some(usa.into());
 
 		{
 			let mut transaction = connection.begin().await.unwrap();

db62c44d9e5a080df49fd9099486fb9d6676d986
Iron-E <code.iron.e@gmail.com>
2022-06-10 15:46:26 -0400

    test(PgLocation): write deletable test

--- crates/adapters/clinvoice_adapter_postgres/src/schema/location/deletable.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/location/deletable.rs
@@ -32,9 +32,43 @@ impl Deletable for PgLocation
 #[cfg(test)]
 mod tests
 {
+	use clinvoice_adapter::{schema::LocationAdapter, Deletable};
+	use clinvoice_match::{Match, MatchLocation};
+
+	use crate::schema::{util, PgLocation};
+
+	// TODO: use fuzzing
 	#[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 	async fn delete()
 	{
-		// TODO: write test
+		let connection = util::connect().await;
+
+		let earth = PgLocation::create(&connection, "Earth".into(), None)
+			.await
+			.unwrap();
+
+		let (chile, usa) = futures::try_join!(
+			PgLocation::create(&connection, "Chile".into(), Some(earth.clone())),
+			PgLocation::create(&connection, "Arizona".into(), Some(earth.clone())),
+		)
+		.unwrap();
+
+		assert!(PgLocation::delete(&connection, [&earth].into_iter())
+			.await
+			.is_err());
+		PgLocation::delete(&connection, [&chile, &usa].into_iter())
+			.await
+			.unwrap();
+
+		assert_eq!(
+			PgLocation::retrieve(&connection, &MatchLocation {
+				id: Match::Or(vec![earth.id.into(), chile.id.into(), usa.id.into()]),
+				..Default::default()
+			})
+			.await
+			.unwrap()
+			.as_slice(),
+			&[earth]
+		);
 	}
 }

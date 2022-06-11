4754b52286cd60511693b6454b4ba4c644f9b932
Iron-E <code.iron.e@gmail.com>
2022-06-11 14:41:05 -0400

    test(PgExpenses): `delete`

--- crates/adapters/clinvoice_adapter_postgres/src/schema/expenses/deletable.rs
+++ crates/adapters/clinvoice_adapter_postgres/src/schema/expenses/deletable.rs
@@ -32,9 +32,125 @@ impl Deletable for PgExpenses
 #[cfg(test)]
 mod tests
 {
+	use core::time::Duration;
+
+	use clinvoice_adapter::{
+		schema::{
+			EmployeeAdapter,
+			ExpensesAdapter,
+			JobAdapter,
+			LocationAdapter,
+			OrganizationAdapter,
+			TimesheetAdapter,
+		},
+		Deletable,
+	};
+	use clinvoice_finance::{Currency, ExchangeRates, Exchangeable, Money};
+	use clinvoice_match::{MatchExpense, MatchSet};
+	use clinvoice_schema::{
+		chrono::{TimeZone, Utc},
+		Invoice,
+	};
+
+	use crate::schema::{
+		util,
+		PgEmployee,
+		PgExpenses,
+		PgJob,
+		PgLocation,
+		PgOrganization,
+		PgTimesheet,
+	};
+
+	// TODO: use fuzzing
 	#[tokio::test]
 	async fn delete()
 	{
-		// TODO: write test
+		let connection = util::connect().await;
+
+		let earth = PgLocation::create(&connection, "Earth".into(), None)
+			.await
+			.unwrap();
+
+		let organization =
+			PgOrganization::create(&connection, Vec::new(), earth, "Some Organization".into())
+				.await
+				.unwrap();
+
+		let employee = PgEmployee::create(
+			&connection,
+			"My Name".into(),
+			organization.clone(),
+			"Employed".into(),
+			"Janitor".into(),
+		)
+		.await
+		.unwrap();
+
+		let job = PgJob::create(
+			&connection,
+			organization.clone(),
+			None,
+			Utc.ymd(1990, 07, 12).and_hms(14, 10, 00),
+			Duration::from_secs(900),
+			Invoice {
+				date: None,
+				hourly_rate: Money::new(20_00, 2, Currency::USD),
+			},
+			String::new(),
+			"Do something".into(),
+		)
+		.await
+		.unwrap();
+
+		let timesheet = PgTimesheet::create(
+			&connection,
+			employee.clone(),
+			vec![
+				(
+					"Flight".into(),
+					Money::new(300_56, 2, Currency::JPY),
+					"Trip to Hawaii for research".into(),
+				),
+				(
+					"Food".into(),
+					Money::new(10_17, 2, Currency::USD),
+					"Takeout".into(),
+				),
+				(
+					"Taxi".into(),
+					Money::new(563_30, 2, Currency::NOK),
+					"Took a taxi cab".into(),
+				),
+			],
+			job,
+			Utc.ymd(2022, 06, 08).and_hms(15, 27, 00),
+			Some(Utc.ymd(2022, 06, 09).and_hms(07, 00, 00)),
+		)
+		.await
+		.unwrap();
+
+		PgExpenses::delete(
+			&connection,
+			[&timesheet.expenses[0], &timesheet.expenses[1]].into_iter(),
+		)
+		.await
+		.unwrap();
+
+		let exchange_rates = ExchangeRates::new().await.unwrap();
+
+		assert_eq!(
+			PgExpenses::retrieve(
+				&connection,
+				&MatchSet::Contains(MatchExpense {
+					timesheet_id: timesheet.id.into(),
+					..Default::default()
+				})
+			)
+			.await
+			.unwrap()[&timesheet.id]
+				.as_slice(),
+			&[timesheet.expenses[2].exchange_ref(Default::default(), &exchange_rates)],
+		);
 	}
 }

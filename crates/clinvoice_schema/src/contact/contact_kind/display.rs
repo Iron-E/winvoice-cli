b9e945d534ae879f5f5af623719b49f9d22401f7
Iron-E <code.iron.e@gmail.com>
2022-06-17 16:38:29 -0400

    style(ContactKind): cargo fmt

--- crates/clinvoice_schema/src/contact/contact_kind/display.rs
+++ crates/clinvoice_schema/src/contact/contact_kind/display.rs
@@ -9,10 +9,7 @@ impl Display for ContactKind
 		match self
 		{
 			Self::Address(ref location) => location.fmt(formatter),
-			Self::Email(ref s) | Self::Other(ref s) | Self::Phone(ref s) =>
-			{
-				s.fmt(formatter)
-			},
+			Self::Email(ref s) | Self::Other(ref s) | Self::Phone(ref s) => s.fmt(formatter),
 		}
 	}
 }

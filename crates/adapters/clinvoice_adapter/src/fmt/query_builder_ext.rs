a26253d23a10db24ccdaf3411f36f6110e123a04
Iron-E <code.iron.e@gmail.com>
2022-06-21 18:39:49 -0400

    feat(QueryBuilderExt): push more types of clauses

--- crates/adapters/clinvoice_adapter/src/fmt/query_builder_ext.rs
+++ crates/adapters/clinvoice_adapter/src/fmt/query_builder_ext.rs
@@ -6,9 +6,33 @@ pub trait QueryBuilderExt
 {
 	/// # Summary
 	///
-	/// Push `left = right`.
+	/// Push `" FROM {table_ident} {table_alias}"`.
+	fn push_from<TAlias, TIdent>(&mut self, table_ident: TIdent, table_alias: TAlias) -> &mut Self
+	where
+		TAlias: Display,
+		TIdent: Display;
+
+	/// # Summary
+	///
+	/// Push `"left = right"`.
 	fn push_equal<TLeft, TRight>(&mut self, left: TLeft, right: TRight) -> &mut Self
 	where
+		TLeft: Display,
+		TRight: Display;
+
+	/// # Summary
+	///
+	/// Push `" JOIN {table_ident} {table_alias} ON ({left} = {right})"`.
+	fn push_equijoin<TAlias, TIdent, TLeft, TRight>(
+		&mut self,
+		table_ident: TIdent,
+		table_alias: TAlias,
+		left: TLeft,
+		right: TRight,
+	) -> &mut Self
+	where
+		TAlias: Display,
+		TIdent: Display,
 		TLeft: Display,
 		TRight: Display;
 }
@@ -24,5 +48,42 @@ where
 	{
 		self.separated('=').push(left).push(right);
 		self
+	}
+
+	fn push_from<TAlias, TIdent>(&mut self, table_ident: TIdent, table_alias: TAlias) -> &mut Self
+	where
+		TAlias: Display,
+		TIdent: Display,
+	{
+		self
+			.separated(' ')
+			.push(" FROM")
+			.push(table_ident)
+			.push(table_alias);
+
+		self
+	}
+
+	fn push_equijoin<TAlias, TIdent, TLeft, TRight>(
+		&mut self,
+		table_ident: TIdent,
+		table_alias: TAlias,
+		left: TLeft,
+		right: TRight,
+	) -> &mut Self
+	where
+		TAlias: Display,
+		TIdent: Display,
+		TLeft: Display,
+		TRight: Display,
+	{
+		self
+			.separated(' ')
+			.push(" JOIN")
+			.push(table_ident)
+			.push(table_alias)
+			.push("ON (");
+
+		self.push_equal(left, right).push(')')
 	}
 }

43168a6052f8853f97d8e1b056a3677cf4a5a80a
Iron-E <code.iron.e@gmail.com>
2022-07-05 23:19:15 -0400

    docs(Match): improve

--- crates/clinvoice_match/src/match.rs
+++ crates/clinvoice_match/src/match.rs
@@ -7,59 +7,69 @@ use core::{cmp::Eq, fmt::Debug};
 #[cfg(feature = "serde_support")]
 use serde::{Deserialize, Serialize};
 
-/// # Summary
+/// A value which describes the condition which some value of type `T` must meet in order to
+/// "_match_".
 ///
-/// A value in a retrieval operation.
+/// # Examples
+///
+/// ```rust
+/// use clinvoice_match::Match;
+///
+/// // this is an example for how a match should be interpreted
+/// fn matches(condition: Match<isize>, x: isize) -> bool {
+///   match condition {
+///     Match::And(conditions) => conditions.into_iter().all(|c| matches(c, x)),
+///     Match::Any => true,
+///     Match::EqualTo(value) => value == x,
+///     Match::GreaterThan(value) => x > value,
+///     Match::InRange(lower, upper) => lower <= x && x < upper,
+///     Match::LessThan(value) => x < value,
+///     Match::Not(c) => !matches(*c, x),
+///     Match::Or(conditions) => conditions.into_iter().any(|c| matches(c, x)),
+///   }
+/// }
+///
+/// assert!(matches(Match::EqualTo(3), 3));
+/// assert!(matches(Match::InRange(5, 10), 9));
+/// assert!(matches(Match::LessThan(4), 1));
+/// assert!(matches(
+///   Match::Not(Match::Or(vec![Match::GreaterThan(1), Match::LessThan(-1)]).into()),
+///   0,
+/// ));
+/// ```
 #[cfg_attr(feature = "serde_support", derive(Deserialize, Serialize))]
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Match<T>
 {
-	/// # Summary
-	///
-	/// Match if and only if all of the contained [`Match`]es also match.
+	/// Match IFF all contained [`Match`]es also match.
 	And(Vec<Self>),
 
-	/// # Summary
-	///
 	/// Always match.
 	Any,
 
-	/// # Summary
-	///
-	/// For some value `v`, match if and only if `v` equals this value.
+	/// Match IFF some value `v` matches the contained value.
 	EqualTo(T),
 
-	/// # Summary
-	///
-	/// For some value `v`, match if and only if `v` is greater than this value.
+	/// Match IFF some value `v` is greater than  (`>`) this value.
 	GreaterThan(T),
 
-	/// # Summary
-	///
-	/// For some value `v`, match if and only if `v` is greater than or equal to the first value and `v` is less than the second value.
+	/// Match IFF some value `v` is greater-than-or-equal-to (`>=`) the left-hand contained value, but is
+	/// less than (`<`) the right-hand contained value.
 	InRange(T, T),
 
-	/// # Summary
-	///
-	/// For some value `v`, match if and only if `v` is less than this value.
+	/// Match IFF some value `v` is less than  (`>`) this value.
 	LessThan(T),
 
-	/// # Summary
-	///
-	/// Negate a [`Match`].
+	/// Match IFF the contained [`Match`] does _not_ match.
 	Not(Box<Self>),
 
-	/// # Summary
-	///
-	/// Match if and only if any of the contained [`Match`]es also match.
+	/// Match IFF any contained [`Match`]es also matches.
 	Or(Vec<Self>),
 }
 
 impl<T> Match<T>
 {
-	/// # Summary
-	///
-	/// Transform some `Match` of type `T` into another type `U` by providing a mapping function.
+	/// Transform some `Match` of type `T` into another type `U` by providing a mapping `f`unction.
 	///
 	/// # See also
 	///
@@ -85,9 +95,7 @@ impl<T> Match<T>
 		}
 	}
 
-	/// # Summary
-	///
-	/// Transform some `Match` of type `T` into another type `U` by providing a mapping function.
+	/// Transform some `Match` of type `T` into another type `U` by providing a mapping `f`unction.
 	///
 	/// # See also
 	///

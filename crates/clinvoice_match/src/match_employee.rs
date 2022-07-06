132a5c0ac066baf49346a4867abbd8f62913b36b
Iron-E <code.iron.e@gmail.com>
2022-07-06 19:02:53 -0400

    docs(MatchEmployee): improve

--- crates/clinvoice_match/src/match_employee.rs
+++ crates/clinvoice_match/src/match_employee.rs
@@ -4,9 +4,28 @@ use serde::{Deserialize, Serialize};
 
 use super::{Match, MatchStr};
 
-/// # Summary
+/// A [`Employee`](clinvoice_schema::Employee) with [matchable](clinvoice_match) fields.
 ///
-/// An [`Employee`](clinvoice_schema::Employee) with [matchable](Match) fields.
+/// [`MatchEmployee`] matches IFF all of its fields also match.
+///
+/// # Examples
+///
+/// ## YAML
+///
+/// Requires the `serde_support` feature. If any field is omitted, it will be set to the
+/// [`Default`] for its type.
+///
+/// ```rust
+/// # assert!(serde_yaml::from_str::<clinvoice_match::MatchEmployee>(r#"
+/// id: any
+/// name:
+///   regex: 'son\b'
+/// status:
+///   equal_to: "Hired"
+/// title:
+///   contains: "C"
+/// # "#).is_ok());
+/// ```
 #[cfg_attr(feature = "serde_support", derive(Deserialize, Serialize))]
 #[derive(Clone, Default, Debug, Eq, PartialEq)]
 pub struct MatchEmployee

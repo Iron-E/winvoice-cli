96c2971282d7b27afc1ab5947f2774f8eb12b2d1
Iron-E <code.iron.e@gmail.com>
2022-07-07 00:47:17 -0400

    docs(MatchOrganization): improve

--- crates/clinvoice_match/src/match_organization.rs
+++ crates/clinvoice_match/src/match_organization.rs
@@ -4,9 +4,29 @@ use serde::{Deserialize, Serialize};
 
 use super::{Match, MatchLocation, MatchStr};
 
-/// # Summary
+/// A [`Organization`](clinvoice_schema::Organization) with [matchable](clinvoice_match) fields.
 ///
-/// An [`Organization`](clinvoice_schema::Organization) with [matchable](Match) fields.
+/// [`MatchOrganization`] matches IFF all of its fields also match.
+///
+/// # Examples
+///
+/// ## YAML
+///
+/// Requires the `serde_support` feature. If any field is omitted, it will be set to the
+/// [`Default`] for its type.
+///
+/// ```rust
+/// # assert!(serde_yaml::from_str::<clinvoice_match::MatchOrganization>(r#"
+/// id: any
+/// location:
+///   outer:
+///     some:
+///       name:
+///         equal_to: "Mexico"
+/// name:
+///   equal_to: "Some Company"
+/// # "#).is_ok());
+/// ```
 #[cfg_attr(feature = "serde_support", derive(Deserialize, Serialize))]
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct MatchOrganization

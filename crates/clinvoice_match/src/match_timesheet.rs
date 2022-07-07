2bbafae0168db31fa7784f5cf92324ff2bcf3696
Iron-E <code.iron.e@gmail.com>
2022-07-07 01:17:24 -0400

    docs(MatchTimesheet): improve

--- crates/clinvoice_match/src/match_timesheet.rs
+++ crates/clinvoice_match/src/match_timesheet.rs
@@ -7,9 +7,37 @@ use serde::{Deserialize, Serialize};
 use super::{Match, MatchEmployee, MatchExpense, MatchJob, MatchSet, MatchStr};
 use crate::MatchOption;
 
-/// # Summary
+/// A [`Timesheet`](clinvoice_schema::Timesheet) with [matchable](clinvoice_match) fields.
 ///
-/// An [`Timesheet`](clinvoice_schema::Timesheet) with [matchable](Match) fields.
+/// [`MatchTimesheet`] matches IFF all of its fields also match.
+///
+/// # Examples
+///
+/// ## YAML
+///
+/// Requires the `serde_support` feature. If any field is omitted, it will be set to the
+/// [`Default`] for its type.
+///
+/// ```rust
+/// # assert!(serde_yaml::from_str::<clinvoice_match::MatchTimesheet>(r#"
+/// id: any
+/// employee:
+///   name:
+///     regex: '^[JR]on$'
+/// expenses:
+///   contains:
+///     category:
+///       equal_to: "Travel"
+/// job:
+///   client:
+///     name:
+///       contains: "Interational"
+/// time_begin:
+///   less_than: "2022-01-01T00:00:00"
+/// time_end: none
+/// work_notes: any
+/// # "#).is_ok());
+/// ```
 #[cfg_attr(feature = "serde_support", derive(Deserialize, Serialize))]
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct MatchTimesheet

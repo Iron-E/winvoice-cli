c4d3f1dc6cb6d746de6ba187bb01c0e10f0e755d
Iron-E <code.iron.e@gmail.com>
2022-07-08 13:20:38 -0400

    ref(clinvoice_error): flatten crate

--- crates/clinvoice_error/src/lib.rs
+++ crates/clinvoice_error/src/lib.rs
@@ -1,4 +1,6 @@
 //! This crate adds macros which generate boilerplate code associated with [`Error`] and [`Result`]
 //! types.
 
-mod macros;
+#![macro_use]
+
+mod alias_result;
--- crates/clinvoice_error/src/macros/mod.rs
+++
@@ -1,7 +0,0 @@
-//! # Summary
-//!
-//! This module exports macros which allow for easier creation of custom [`Error`] types.
-
-#![macro_use]
-
-mod alias_result;

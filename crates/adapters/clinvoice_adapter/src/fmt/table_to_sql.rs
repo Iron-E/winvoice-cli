42da8ddf3c21263970942958e3c40396b61e15cf
Iron-E <code.iron.e@gmail.com>
2022-07-09 00:36:25 -0400

    docs(TableToSql): improve

--- crates/adapters/clinvoice_adapter/src/fmt/table_to_sql.rs
+++ crates/adapters/clinvoice_adapter/src/fmt/table_to_sql.rs
@@ -1,10 +1,29 @@
-/// # Summary
-///
-/// Defines attributes of a table which is part of the CLInvoice schema.
-pub trait TableToSql
+pub(crate) mod sealed
 {
-	/// # Summary
-	///
+	use crate::schema::columns::{
+		ContactColumns,
+		EmployeeColumns,
+		ExpenseColumns,
+		JobColumns,
+		LocationColumns,
+		OrganizationColumns,
+		TimesheetColumns,
+	};
+
+	pub trait Sealed {}
+	impl<T> Sealed for ContactColumns<T> {}
+	impl<T> Sealed for EmployeeColumns<T> {}
+	impl<T> Sealed for ExpenseColumns<T> {}
+	impl<T> Sealed for JobColumns<T> {}
+	impl<T> Sealed for LocationColumns<T> {}
+	impl<T> Sealed for OrganizationColumns<T> {}
+	impl<T> Sealed for TimesheetColumns<T> {}
+}
+
+/// Defines attributes of a table in a database which was
+/// [`init`](crate::Initializable::init)ialized for use with CLInvoice.
+pub trait TableToSql: sealed::Sealed
+{
 	/// Get the standard alias that can be used to refer to this table.
 	///
 	/// # Warnings
@@ -12,8 +31,6 @@ pub trait TableToSql
 	/// * Must be unique among other implementors of [`TableToSql`].
 	const DEFAULT_ALIAS: char;
 
-	/// # Summary
-	///
 	/// Get the name of this table.
 	///
 	/// # Warnings

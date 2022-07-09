e65ba6c1879fd8cb9a1ffa1a695f92899e836eac
Iron-E <code.iron.e@gmail.com>
2022-07-09 13:04:31 -0400

    docs(ExpenseColumns): improve

--- crates/adapters/clinvoice_adapter/src/schema/columns/expense.rs
+++ crates/adapters/clinvoice_adapter/src/schema/columns/expense.rs
@@ -3,22 +3,51 @@ mod table_to_sql;
 
 use crate::fmt::{As, TableToSql, TypeCast, WithIdentifier};
 
+/// The names of the columns of the `expenses` table.
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct ExpenseColumns<T>
 {
+	/// The name of the `category` column of the `expenses` table.
 	pub category: T,
+
+	/// The name of the `cost` column of the `expenses` table.
 	pub cost: T,
+
+	/// The name of the `description` column of the `expenses` table.
 	pub description: T,
+
+	/// The name of the `id` column of the `expenses` table.
 	pub id: T,
+
+	/// The name of the `timesheet_id` column of the `expenses` table.
 	pub timesheet_id: T,
 }
 
 impl<T> ExpenseColumns<T>
 {
-	/// # Summary
+	/// Returns a [`ExpenseColumns`] which aliases the names of these [`ExpenseColumns`] with the
+	/// `aliased` columns provided.
 	///
-	/// Returns a [`ExpenseColumns`] which outputs all of its columns as
-	/// `column_1 AS aliased_column_1`.
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::schema::columns::ExpenseColumns;
+	///
+	/// assert_eq!(
+	///   ExpenseColumns::default()
+	///     .default_scope()
+	///     .r#as(ExpenseColumns {
+	///       category: "one",
+	///       cost: "two",
+	///       description: "three",
+	///       id: "four",
+	///       timesheet_id: "five",
+	///     })
+	///     .id
+	///     .to_string(),
+	///   "X.id AS four",
+	/// );
+	/// ```
 	pub fn r#as<TAlias>(self, aliased: ExpenseColumns<TAlias>) -> ExpenseColumns<As<T, TAlias>>
 	{
 		ExpenseColumns {
@@ -30,18 +59,36 @@ impl<T> ExpenseColumns<T>
 		}
 	}
 
-	/// # Summary
+	/// Add a [scope](ExpenseColumns::scope) using the [default alias](TableToSql::default_alias)
 	///
-	/// Add a [scope](Self::scope) using the [default alias](TableToSql::default_alias)
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::schema::columns::ExpenseColumns;
+	///
+	/// assert_eq!(
+	///   ExpenseColumns::default().default_scope().id.to_string(),
+	///   "X.id",
+	/// );
+	/// ```
 	pub fn default_scope(self) -> ExpenseColumns<WithIdentifier<char, T>>
 	{
 		self.scope(Self::DEFAULT_ALIAS)
 	}
 
-	/// # Summary
-	///
 	/// Returns a [`ExpenseColumns`] which modifies its fields' [`Display`]
-	/// implementation to output `{ident}.{column}`.
+	/// implementation to output `{alias}.{column}`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::schema::columns::ExpenseColumns;
+	///
+	/// assert_eq!(
+	///   ExpenseColumns::default().scope('X').id.to_string(),
+	///   "X.id",
+	/// );
+	/// ```
 	pub fn scope<TAlias>(self, alias: TAlias) -> ExpenseColumns<WithIdentifier<TAlias, T>>
 	where
 		TAlias: Copy,
@@ -55,10 +102,19 @@ impl<T> ExpenseColumns<T>
 		}
 	}
 
-	/// # Summary
-	///
 	/// Returns a [`ExpenseColumns`] which modifies its fields' [`Display`]
 	/// implementation to output `{column}::{cast}`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::schema::columns::ExpenseColumns;
+	///
+	/// assert_eq!(
+	///   ExpenseColumns::default().typecast("numeric").cost.to_string(),
+	///   " CAST (cost AS numeric)",
+	/// );
+	/// ```
 	pub fn typecast<TCast>(self, cast: TCast) -> ExpenseColumns<TypeCast<T, TCast>>
 	where
 		TCast: Copy,
@@ -75,6 +131,7 @@ impl<T> ExpenseColumns<T>
 
 impl ExpenseColumns<&'static str>
 {
+	/// The names of the columns in `expenses` without any aliasing.
 	pub const fn default() -> Self
 	{
 		Self {
@@ -86,6 +143,52 @@ impl ExpenseColumns<&'static str>
 		}
 	}
 
+	/// Aliases for the columns in `employees` which are guaranteed to be unique among other [`columns`](super)'s `unique` aliases.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::{
+	///   fmt::{QueryBuilderExt, sql},
+	///   schema::columns::{ExpenseColumns, OrganizationColumns},
+	/// };
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// {
+	///   let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	///   let sql = query
+	///     .push_columns(&ExpenseColumns::default().default_scope())
+	///     .push_more_columns(&OrganizationColumns::default().default_scope())
+	///     .prepare()
+	///     .sql();
+	///
+	///   // `sqlx::Row::get` ignores scopes (e.g. "X." in "X.id") so "X.id" and "O.id" clobber each
+	///   // other.
+	///   assert_eq!(sql,
+	///     " SELECT X.category,X.cost,X.description,X.id,X.timesheet_id,O.id,O.location_id,O.name;"
+	///   );
+	/// }
+	///
+	/// {
+	///   let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	///   let sql = query
+	///     .push_columns(&ExpenseColumns::default().default_scope().r#as(ExpenseColumns::unique()))
+	///     .push_more_columns(&OrganizationColumns::default().default_scope())
+	///     .prepare()
+	///     .sql();
+	///
+	///   // no clobbering
+	///   assert_eq!(sql,
+	///     " SELECT \
+	///         X.category AS unique_3_expense_category,\
+	///         X.cost AS unique_3_expense_cost,\
+	///         X.description AS unique_3_expense_description,\
+	///         X.id AS unique_3_expense_id,\
+	///         X.timesheet_id AS unique_3_expense_timesheet_id,\
+	///         O.id,O.location_id,O.name;"
+	///   );
+	/// }
+	/// ```
 	pub const fn unique() -> Self
 	{
 		Self {

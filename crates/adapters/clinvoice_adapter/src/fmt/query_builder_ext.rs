da94a325ee0768280d3b908a1ff2302d68b770d6
Iron-E <code.iron.e@gmail.com>
2022-07-09 00:21:49 -0400

    docs(QueryBuilderExt): improve

--- crates/adapters/clinvoice_adapter/src/fmt/query_builder_ext.rs
+++ crates/adapters/clinvoice_adapter/src/fmt/query_builder_ext.rs
@@ -20,14 +20,79 @@ pub trait QueryBuilderExt<'args>: sealed::Sealed
 	type Db: Database;
 
 	/// Add a semicolon to the end of the current query and then [build](QueryBuilder::build) it.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::fmt::{QueryBuilderExt, sql, WithIdentifier};
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	/// let sql = query
+	///   .push(WithIdentifier('F', "bar"))
+	///   .push_from("foo", 'F')
+	///   .prepare()
+	///   .sql();
+	///
+	/// assert_eq!(sql.chars().last(), Some(';'));
+	/// ```
 	fn prepare(&mut self) -> Query<Self::Db, <Self::Db as HasArguments<'args>>::Arguments>;
 
 	/// [`ColumnsToSql::push_to`] this query.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::{
+	///   fmt::{QueryBuilderExt, sql, WithIdentifier},
+	///   schema::columns::{ContactColumns, OrganizationColumns},
+	/// };
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	/// let sql = query
+	///   .push_columns(&ContactColumns::default())
+	///   .push_more_columns(&OrganizationColumns::default())
+	///   .prepare()
+	///   .sql();
+	///
+	/// assert_eq!(sql, " SELECT address_id,email,label,other,phone,id,location_id,name;");
+	/// ```
 	fn push_columns<T>(&mut self, columns: &T) -> &mut Self
 	where
 		T: ColumnsToSql;
 
-	/// Push `" JOIN {TTable::TABLE_NAME} {TTable::table_alias()} ON ({left} = {right})"`.
+	/// Push `" JOIN {TTable::TABLE_NAME} {TTable::TABLE_ALIAS} ON ({left} = {right})"`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::{
+	///   fmt::{QueryBuilderExt, sql, WithIdentifier},
+	///   schema::columns::{LocationColumns, OrganizationColumns},
+	/// };
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// let organization_columns = OrganizationColumns::default().default_scope();
+	/// let location_columns = LocationColumns::default().default_scope();
+	///
+	/// let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	/// let sql = query
+	///   .push_columns(&organization_columns)
+	///   .push_default_from::<OrganizationColumns<&str>>()
+	///   .push_default_equijoin::<LocationColumns<&str>, _, _>(
+	///     location_columns.id,
+	///     organization_columns.location_id,
+	///   )
+	///   .prepare()
+	///   .sql();
+	///
+	/// assert_eq!(sql,
+	///   " SELECT O.id,O.location_id,O.name \
+	///     FROM organizations O \
+	///     JOIN locations L ON (L.id=O.location_id);"
+	/// );
+	/// ```
 	fn push_default_equijoin<TTable, TLeft, TRight>(
 		&mut self,
 		left: TLeft,
@@ -41,7 +106,11 @@ pub trait QueryBuilderExt<'args>: sealed::Sealed
 		self.push_equijoin(TTable::TABLE_NAME, TTable::DEFAULT_ALIAS, left, right)
 	}
 
-	/// Push `" FROM {T::TABLE_NAME} {T::table_alias()}"`.
+	/// Push `" FROM {T::TABLE_NAME} {T::TABLE_ALIAS}"`.
+	///
+	/// # Examples
+	///
+	/// * See [`QueryBuilderExt::push_default_equijoin`]
 	fn push_default_from<T>(&mut self) -> &mut Self
 	where
 		T: TableToSql,
@@ -50,12 +119,51 @@ pub trait QueryBuilderExt<'args>: sealed::Sealed
 	}
 
 	/// Push `"left = right"`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::{
+	///   fmt::{QueryBuilderExt, sql, WithIdentifier},
+	///   schema::columns::OrganizationColumns,
+	/// };
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// let organization_columns = OrganizationColumns::default().default_scope();
+	///
+	/// let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	/// let sql = query
+	///   .push_columns(&organization_columns)
+	///   .push_default_from::<OrganizationColumns<&str>>()
+	///   .push(sql::WHERE)
+	///   .push_equal(organization_columns.id, 3)
+	///   .prepare()
+	///   .sql();
+	///
+	/// assert_eq!(sql, " SELECT O.id,O.location_id,O.name FROM organizations O WHERE O.id=3;");
+	/// ```
 	fn push_equal<TLeft, TRight>(&mut self, left: TLeft, right: TRight) -> &mut Self
 	where
 		TLeft: Display,
 		TRight: Display;
 
 	/// Push `" JOIN {table_ident} {table_alias} ON ({left} = {right})"`.
+	///
+	/// # Examples
+	///
+	/// ```rust
+	/// use clinvoice_adapter::fmt::{QueryBuilderExt, sql, WithIdentifier};
+	/// use sqlx::{Execute, Postgres, QueryBuilder};
+	///
+	/// let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+	/// let sql = query
+	///   .push_from("foo", 'F')
+	///   .push_equijoin("bar", 'B', "B.foo_id", "F.id")
+	///   .prepare()
+	///   .sql();
+	///
+	/// assert_eq!(sql, " SELECT  FROM foo F JOIN bar B ON (B.foo_id=F.id);");
+	/// ```
 	fn push_equijoin<TIdent, TAlias, TLeft, TRight>(
 		&mut self,
 		table_ident: TIdent,
@@ -69,17 +177,21 @@ pub trait QueryBuilderExt<'args>: sealed::Sealed
 		TLeft: Display,
 		TRight: Display;
 
-	/// # Summary
-	///
 	/// Push `" FROM {table_ident} {table_alias}"`.
+	///
+	/// # Examples
+	///
+	/// * See [`QueryBuilderExt::push_equijoin`]
 	fn push_from<TIdent, TAlias>(&mut self, table_ident: TIdent, table_alias: TAlias) -> &mut Self
 	where
 		TAlias: Display,
 		TIdent: Display;
 
-	/// # Summary
-	///
 	/// Push a comma and then [`push_columns`](QueryBuilderExt::push_columns).
+	///
+	/// # Examples
+	///
+	/// * See [`QueryBuilderExt::push_columns`]
 	fn push_more_columns<T>(&mut self, columns: &T) -> &mut Self
 	where
 		T: ColumnsToSql;

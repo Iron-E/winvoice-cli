30db5520cdaf6f8d0c9e432c650b8eea5e02e590
Iron-E <code.iron.e@gmail.com>
2022-07-09 11:38:15 -0400

    docs(clinvoice_adapter schema columns): improve

--- crates/adapters/clinvoice_adapter/src/schema/columns/mod.rs
+++ crates/adapters/clinvoice_adapter/src/schema/columns/mod.rs
@@ -1,3 +1,30 @@
+//! This module holds types which represent the columns for every table that CLInvoice requires to
+//! exist in a [`Database`](sqlx::Database).
+//!
+//! # Examples
+//!
+//! ```rust
+//! use clinvoice_adapter::{
+//!   fmt::{QueryBuilderExt, sql},
+//!   schema::columns::LocationColumns,
+//! };
+//! use sqlx::{Execute, Postgres, QueryBuilder};
+//!
+//! let mut query = QueryBuilder::<Postgres>::new(sql::SELECT);
+//! let sql = query
+//!   .push_columns(&LocationColumns::default().default_scope().r#as(LocationColumns {
+//!     id: "aliased_id",
+//!     name: "aliased_name",
+//!     outer_id: "aliased_outer_id",
+//!   }))
+//!   .prepare()
+//!   .sql();
+//!
+//! assert_eq!(sql,
+//!   " SELECT L.id AS aliased_id,L.name AS aliased_name,L.outer_id AS aliased_outer_id;"
+//! );
+//! ```
+
 mod contact;
 mod employee;
 mod expense;

4ff0bd4ef5c626f46712c2dfbde1869b56f418d1
Iron-E <code.iron.e@gmail.com>
2022-07-13 13:08:19 -0400

    ref(RetrieveCommand): flesh out arguments

--- src/args/retrieve/command.rs
+++ src/args/retrieve/command.rs
@@ -1,3 +1,5 @@
+use clinvoice_export::Format;
+use std::path::PathBuf;
 use clap::Subcommand as Clap;
 
 /// The specific type of information that is being retrieved.
@@ -5,12 +7,59 @@ use clap::Subcommand as Clap;
 #[clap(about = "Retrieve information that was recorded with CLInvoice")]
 pub enum RetrieveCommand
 {
-	// TODO: flesh out
+	/// Retrieve `Contact`s from the store (-s) specified.
 	Contact,
-	Employee,
+
+	/// Retrieve `Employee`s from the store (-s) specified.
+	Employee
+	{
+		/// Retrieve the `Employee` specified in the `id` field of the `[employees]` section of the
+		/// CLInvoice configuration file.
+		#[clap(action, default_value_t = false, long, short)]
+		default: bool,
+
+		/// Set the `id` field of the `[employees]` section of the CLInvoice configuration file to
+		/// the `Employee` which was retrieved by this operation.
+		#[clap(action, default_value_t = false, long, short)]
+		set_default: bool,
+	},
+
+	/// Retrieve `Expense`s from the store (-s) specified.
 	Expense,
-	Job,
+
+	/// Retrieve `Job`s from the store (-s) specified.
+	Job
+	{
+		/// Select retrieved `Job`s and export them to a file.
+		#[clap(action, default_value_t = false, long, short)]
+		export: bool,
+
+		/// What file format to `--export` to.
+		#[clap(long, short, requires("export"), value_name = "markdown")]
+		format: Option<Format>,
+
+		/// What directory the `--export`ed files should be placed in.
+		#[clap(long, short, requires("export"), value_name = "DIR", value_parser)]
+		output_dir: Option<PathBuf>,
+	},
+
+	/// Retrieve `Location`s from the store (-s) specified.
 	Location,
-	Organization,
+
+	/// Retrieve `Organization`s from the store (-s) specified.
+	Organization
+	{
+		/// Retrieve the `Organization` specified in the `employer_id` field of the `[organizations]`
+		/// section of the CLInvoice configuration file.
+		#[clap(action, default_value_t = false, long, short)]
+		employer: bool,
+
+		/// Set the `employer_id` field of the `[organizations]` section of the CLInvoice configuration
+		/// file to the `Organization` which was retrieved by this operation.
+		#[clap(action, default_value_t = false, long, short)]
+		set_employer: bool,
+	},
+
+	/// Retrieve `Timesheet`s from the store (-s) specified.
 	Timesheet,
 }

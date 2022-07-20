909d7c1f29530521c52f51d133da9bc30ee0d783
Iron-E <code.iron.e@gmail.com>
2022-07-19 20:54:39 -0400

    feat(MatchArgs): `deserialize`

--- src/args/match_args.rs
+++ src/args/match_args.rs
@@ -1,6 +1,11 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use clap::Args as Clap;
+use clinvoice_config::Error as ConfigError;
+use serde::de::DeserializeOwned;
+use serde_yaml as yaml;
+
+use crate::input::{Error, Result};
 
 /// Reusable arguments used for retrieving information from a CLInvoice store.
 #[derive(Clap, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -14,4 +19,17 @@ pub struct MatchArgs
 	/// for deserialized, and you will not be prompted to write a new search.
 	#[clap(long, short, value_name = "FILE", value_parser)]
 	r#match: Option<PathBuf>,
+}
+
+impl MatchArgs
+{
+	/// Attempt to deserialize this `match` file into a concrete type `T`
+	pub fn deserialize<T>(self) -> Result<Option<T>>
+	where
+		T: DeserializeOwned,
+	{
+		let contents = self.r#match.map(fs::read_to_string).transpose()?;
+		let deserialized = contents.as_deref().map(yaml::from_str).transpose()?;
+		Ok(deserialized)
+	}
 }
